4609603caa9641bea68e7125c680812cb940c59f
gorzelinski <mateusz.gorzelinski@gmail.com>
2021-08-19 18:41:30 +0200

    test: add unit testing support with jest

---
+++ src/components/__tests__/seo.js
@@ -0,0 +1,36 @@
+import React from "react"
+import { render } from "@testing-library/react"
+import { useStaticQuery } from "gatsby"
+
+import Helmet from "react-helmet"
+import SEO from "../seo"
+
+describe("SEO component", () => {
+  beforeAll(() => {
+    useStaticQuery.mockReturnValue({
+      site: {
+        siteMetadata: {
+          title: `Gatsby Starter Blog`,
+          description: `A starter blog demonstrating what Gatsby can do.`,
+          social: {
+            twitter: `kylemathews`,
+          },
+        },
+      },
+    })
+  })
+
+  it("renders the tests correctly", () => {
+    const mockTitle = "All posts | Gatsby Starter Blog"
+    const mockDescription = "A starter blog demonstrating what Gatsby can do."
+    const mockTwitterHandler = "kylemathews"
+
+    render(<SEO title="All posts" />)
+    const { title, metaTags } = Helmet.peek()
+
+    expect(title).toBe(mockTitle)
+    expect(metaTags[0].content).toBe(mockDescription)
+    expect(metaTags[5].content).toBe(mockTwitterHandler)
+    expect(metaTags.length).toBe(8)
+  })
+})
