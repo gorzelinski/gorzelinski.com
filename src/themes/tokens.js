1e8549ca787bcabdbe57b947474a7ad26280fc35
gorzelinski <mateusz.gorzelinski@gmail.com>
2021-08-27 12:12:06 +0200

    refactor: move design tokens to separate file

---
+++ src/themes/tokens.js
@@ -0,0 +1,59 @@
+export const tokens = {
+  font: {
+    family: {
+      heading: "Montserrat, sans-serif",
+      body: "Lora, serif",
+      code: "Fira Code, monospace",
+    },
+    size: {
+      small: "0.875rem",
+      base: "1rem",
+      xs: "1.25rem",
+      s: "1.5rem",
+      m: "2rem",
+      l: "2.5rem",
+      xl: "3rem",
+      xxl: "4rem",
+    },
+    height: {
+      small: "1.25rem",
+      base: "1.5rem",
+      xs: "1.75rem",
+      s: "2rem",
+      m: "2.5rem",
+      l: "3.5rem",
+      xl: "4rem",
+      xxl: "5rem",
+    },
+    spacing: {
+      packed: "-0.05em",
+      narrow: "-0.025em",
+      wide: "0.1em",
+    },
+    weight: {
+      regular: "400",
+      medium: "500",
+    },
+  },
+  screen: {
+    large: "1920px",
+    desktop: "1280px",
+    tablet: "768px",
+    mobile: "480px",
+  },
+  shadow: {
+    neumorphism:
+      "8px 8px 16px rgba(0, 24, 69, 0.15), -8px -8px 16px #FFFFFF, inset 0px 0px 4px rgba(255, 255, 255, 0.2)",
+    outline: "0px 0px 4px 4px #0466C8",
+  },
+  space: {
+    xxs: "0.125rem",
+    xs: "0.5rem",
+    s: "1rem",
+    m: "2rem",
+    l: "4rem",
+    xl: "6.5rem",
+    xxl: "10.5rem",
+    xxxl: "17rem",
+  },
+}
