1a223fde73f6acb3ee7b790d389cac6be8e6f9bc
gorzelinski <mateusz.gorzelinski@gmail.com>
2021-09-01 12:18:02 +0200

    feat: add test content for the portfolio

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,13 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
+        path: `${__dirname}/content/portfolio`,
+        name: `portfolio`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
         name: `images`,
         path: `${__dirname}/src/images`,
       },
