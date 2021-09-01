66f05ee669baeec5efefbb6bbfe0aa1105d688d8
gorzelinski <mateusz.gorzelinski@gmail.com>
2021-09-01 12:20:15 +0200

    refactor: modify query for downloading blog posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -72,7 +72,10 @@ export const pageQuery = graphql`
         title
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { fileAbsolutePath: { regex: "/(blog)/" } }
+    ) {
       nodes {
         excerpt
         fields {
