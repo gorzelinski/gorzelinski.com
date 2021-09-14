64dc75c542f70d0f54030934255b080808a5e3bb
gorzelinski <mateusz.gorzelinski@gmail.com>
2021-09-14 11:05:46 +0200

    feat: add portfolio page

---
+++ src/pages/portfolio.js
@@ -0,0 +1,56 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import { H1, Header, Section } from "../elements"
+import Layout from "../components/layout"
+import Projects from "../components/projects"
+import Seo from "../components/seo"
+
+const Portfolio = ({ data }) => {
+  return (
+    <Layout>
+      <Seo title="Portfolio"></Seo>
+      <Section>
+        <Header>
+          <H1>Wszystkie projekty</H1>
+        </Header>
+        <Projects data={data}></Projects>
+      </Section>
+    </Layout>
+  )
+}
+
+export default Portfolio
+
+export const pageQuery = graphql`
+  query AllPortfolioProjects {
+    allMarkdownRemark(
+      filter: { fileAbsolutePath: { regex: "/(portfolio)/" } }
+      sort: { fields: frontmatter___date, order: DESC }
+    ) {
+      nodes {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          client
+          date
+          description
+          myRole
+          title
+          tools
+          live
+          featuredImage {
+            alt
+            src {
+              childImageSharp {
+                gatsbyImageData
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
