b47199cfaafe05434ec7dc2ea8fb05631e9b030c
gorzelinski <mateusz.gorzelinski@gmail.com>
2021-09-23 12:51:35 +0200

    style: delete comments from layout component

--- src/components/layout.js
+++ src/components/layout.js
@@ -16,24 +16,6 @@ const Layout = ({ children }) => {
     setSelectedTheme(theme)
   }, [themeLoaded, theme])
 
-  // const rootPath = `${__PATH_PREFIX__}/`
-  // const isRootPath = location.pathname === rootPath
-  // let header
-
-  // if (isRootPath) {
-  //   header = (
-  //     <h1 className="main-heading">
-  //       <Link to="/">{title}</Link>
-  //     </h1>
-  //   )
-  // } else {
-  //   header = (
-  //     <Link className="header-link-home" to="/">
-  //       {title}
-  //     </Link>
-  //   )
-  // }
-
   return (
     themeLoaded && (
       <ThemeProvider theme={selectedTheme}>
@@ -50,15 +32,6 @@ const Layout = ({ children }) => {
               ></Navbar>
               <main>{children}</main>
               <Footer></Footer>
-              {/* <div className="global-wrapper" data-is-root-path={isRootPath}>
-                <header className="global-header">{header}</header>
-                <main>{children}</main>
-                <footer>
-                  © {new Date().getFullYear()}, Built with
-                  {` `}
-                  <a href="https://www.gatsbyjs.com">Gatsby</a>
-                </footer>
-              </div> */}
             </Wrapper>
           </Background>
         </MDXProvider>
