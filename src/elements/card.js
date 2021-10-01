18c5b2f8ef75ee010f3943a1f7ab2a34a4cc4616
gorzelinski <mateusz.gorzelinski@gmail.com>
2021-10-01 14:16:01 +0200

    fix: delete unnecessary prop in card element

--- src/elements/card.js
+++ src/elements/card.js
@@ -1,6 +1,5 @@
 import styled, { css } from "styled-components"
 
-import { media } from "./utils"
 import {
   twoEights,
   half,
@@ -22,31 +21,5 @@ export const Card = styled.article`
     props.$textCentered &&
     css`
       text-align: center;
-    `}
-
-  ${props =>
-    props.$read &&
-    css`
-      grid-column: span 8;
-
-      ${media.mobile`
-        grid-column-start: 2;
-        grid-column-end: span 7;
-      `}
-
-      ${media.tablet`
-        grid-column-start: 2;
-        grid-column-end: span 6;
-      `}
-
-      ${media.desktop`
-        grid-column-start: 2;
-        grid-column-end: span 5;
-      `}
-
-      ${media.large`
-        grid-column-start: 2;
-        grid-column-end: span 4;
-      `}
     `}
 `
