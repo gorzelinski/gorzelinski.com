deac0dc788d11617ed9e12df9dc600c5c50d5971
gorzelinski <mateusz.gorzelinski@gmail.com>
2021-10-12 15:09:02 +0200

    feat: add typing animation in landing section

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,4 +1,5 @@
 import React from "react"
+import TypewriterComponent from "typewriter-effect"
 
 import { Hero, H1, P, Button } from "../elements"
 
@@ -7,14 +8,33 @@ const Landing = () => {
     <Hero>
       <div>
         <H1 $top>
-          Tworzę
-          <br />
+          <TypewriterComponent
+            onInit={typewriter => {
+              typewriter
+                .changeDelay(75)
+                .pauseFor(250)
+                .typeString("Projektuję")
+                .pauseFor(500)
+                .deleteAll()
+                .typeString("Koduję")
+                .pauseFor(500)
+                .deleteAll()
+                .typeString("Publikuję")
+                .pauseFor(500)
+                .deleteAll()
+                .typeString("Hmm...")
+                .pauseFor(1000)
+                .deleteAll()
+                .typeString("Tworzę")
+                .start()
+            }}
+          />
           rzeczy w internecie
         </H1>
         <P $lead>
-          Pomagam małym firmom i jednostkom wkroczyć w XXI wiek. Tworzę dla nich
-          wydajne i spersonalizowane strony internetowe, które dbają o
-          profesjonalny wizerunek online.
+          Pomagam małym firmom i jednostkom wkroczyć w XXI wiek, tworząc dla
+          nich spersonalizowane strony internetowe, które dbają o profesjonalny
+          wizerunek online.
         </P>
         <Button $primary $grow to="#kontakt">
           Stwórzmy coś razem
