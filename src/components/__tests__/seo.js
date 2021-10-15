57e25695a5bca54a29fbf65e2ad2ec2826737043
gorzelinski <mateusz.gorzelinski@gmail.com>
2021-10-15 13:24:46 +0200

    feat: add tests for seo component

--- src/components/__tests__/seo.js
+++ src/components/__tests__/seo.js
@@ -10,27 +10,251 @@ describe("SEO component", () => {
     useStaticQuery.mockReturnValue({
       site: {
         siteMetadata: {
-          title: `Gatsby Starter Blog`,
-          description: `A starter blog demonstrating what Gatsby can do.`,
+          title: `Gorzeliński`,
+          description: `Default meta description`,
+          siteUrl: `https://gorzelinski.com`,
           social: {
-            twitter: `kylemathews`,
+            twitter: `gorzelinski`,
           },
         },
       },
     })
   })
 
-  it("renders the tests correctly", () => {
-    const mockTitle = "All posts | Gatsby Starter Blog"
-    const mockDescription = "A starter blog demonstrating what Gatsby can do."
-    const mockTwitterHandler = "@kylemathews"
-    // TODO: add more test cases
-    render(<SEO title="All posts" />)
-    const { title, metaTags } = Helmet.peek()
+  const mockDefaultDescription = "Default meta description"
+  const mockCustomDescription = "Custom meta description"
+  const mockDefaultTitle = "Gorzeliński"
+  const mockTitle = "Custom title"
+  const mockTitleTemplate = "Custom title | Gorzeliński"
+  const mockDefaultUrl = "https://gorzelinski.com"
+  const mockCustomUrl = "https://gorzelinski.com/custom"
+  const mockDefaultType = "website"
+  const mockCustomType = "article"
+  const mockTwitterHandler = "@gorzelinski"
+  const mockImage = {
+    src: "https://gorzelinski.com/slug-to-image",
+    alt: "Alt text",
+    width: 1200,
+    height: 628,
+  }
 
-    expect(title).toBe(mockTitle)
-    expect(metaTags[0].content).toBe(mockDescription)
-    expect(metaTags[8].content).toBe(mockTwitterHandler)
-    expect(metaTags.length).toBe(12)
+  describe("base tags", () => {
+    it("title", () => {
+      render(<SEO title="Custom title" />)
+      const { title } = Helmet.peek()
+      expect(title).toBe(mockTitleTemplate)
+    })
+
+    it("default description", () => {
+      render(<SEO title="Custom title" />)
+      const { metaTags } = Helmet.peek()
+      expect(metaTags[0].content).toBe(mockDefaultDescription)
+    })
+
+    it("custom description", () => {
+      render(<SEO title="Custom title" description="Custom meta description" />)
+      const { metaTags } = Helmet.peek()
+      expect(metaTags[0].content).toBe(mockCustomDescription)
+    })
+  })
+
+  describe("og tags", () => {
+    it("og:site_name", () => {
+      render(<SEO title="Custom title" />)
+      const { metaTags } = Helmet.peek()
+      expect(metaTags[1].content).toBe(mockDefaultTitle)
+    })
+
+    it("og:url (base)", () => {
+      render(<SEO title="Custom title" />)
+      const { metaTags } = Helmet.peek()
+      expect(metaTags[2].content).toBe(mockDefaultUrl)
+    })
+
+    it("og:url (custom)", () => {
+      render(<SEO title="Custom title" slug="/custom" />)
+      const { metaTags } = Helmet.peek()
+      expect(metaTags[2].content).toBe(mockCustomUrl)
+    })
+
+    it("og:type (default)", () => {
+      render(<SEO title="Custom title" />)
+      const { metaTags } = Helmet.peek()
+      expect(metaTags[3].content).toBe(mockDefaultType)
+    })
+
+    it("og:type (custom)", () => {
+      render(<SEO title="Custom title" type="article" />)
+      const { metaTags } = Helmet.peek()
+      expect(metaTags[3].content).toBe(mockCustomType)
+    })
+
+    it("og:title", () => {
+      render(<SEO title="Custom title" />)
+      const { metaTags } = Helmet.peek()
+      expect(metaTags[4].content).toBe(mockTitle)
+    })
+
+    it("og:description", () => {
+      render(<SEO title="Custom title" />)
+      const { metaTags } = Helmet.peek()
+      expect(metaTags[5].content).toBe(mockDefaultDescription)
+    })
+
+    it("og:image", () => {
+      render(
+        <SEO
+          title="Custom title"
+          image={{
+            src: "/slug-to-image",
+            alt: "Alt text",
+            width: 1200,
+            height: 628,
+          }}
+        />
+      )
+      const { metaTags } = Helmet.peek()
+      expect(metaTags[12].content).toBe(mockImage.src)
+    })
+
+    it("og:image:alt", () => {
+      render(
+        <SEO
+          title="Custom title"
+          image={{
+            src: "/slug-to-image",
+            alt: "Alt text",
+            width: 1200,
+            height: 628,
+          }}
+        />
+      )
+      const { metaTags } = Helmet.peek()
+      expect(metaTags[13].content).toBe(mockImage.alt)
+    })
+
+    it("og:image:wdith", () => {
+      render(
+        <SEO
+          title="Custom title"
+          image={{
+            src: "/slug-to-image",
+            alt: "Alt text",
+            width: 1200,
+            height: 628,
+          }}
+        />
+      )
+      const { metaTags } = Helmet.peek()
+      expect(metaTags[14].content).toBe(mockImage.width)
+    })
+
+    it("og:image:height", () => {
+      render(
+        <SEO
+          title="Custom title"
+          image={{
+            src: "/slug-to-image",
+            alt: "Alt text",
+            width: 1200,
+            height: 628,
+          }}
+        />
+      )
+      const { metaTags } = Helmet.peek()
+      expect(metaTags[15].content).toBe(mockImage.height)
+    })
+  })
+
+  describe("twitter tags", () => {
+    it("twitter:url (base)", () => {
+      render(<SEO title="Custom title" />)
+      const { metaTags } = Helmet.peek()
+      expect(metaTags[6].content).toBe(mockDefaultUrl)
+    })
+
+    it("twitter:url (custom)", () => {
+      render(<SEO title="Custom title" slug="/custom" />)
+      const { metaTags } = Helmet.peek()
+      expect(metaTags[6].content).toBe(mockCustomUrl)
+    })
+
+    it("twitter:site", () => {
+      render(<SEO title="Custom title" />)
+      const { metaTags } = Helmet.peek()
+      expect(metaTags[7].content).toBe(mockTwitterHandler)
+    })
+
+    it("twitter:creator", () => {
+      render(<SEO title="Custom title" />)
+      const { metaTags } = Helmet.peek()
+      expect(metaTags[8].content).toBe(mockTwitterHandler)
+    })
+
+    it("twitter:title", () => {
+      render(<SEO title="Custom title" />)
+      const { metaTags } = Helmet.peek()
+      expect(metaTags[9].content).toBe(mockTitle)
+    })
+
+    it("twitter:description", () => {
+      render(<SEO title="Custom title" />)
+      const { metaTags } = Helmet.peek()
+      expect(metaTags[10].content).toBe(mockDefaultDescription)
+    })
+
+    it("twitter:card (default)", () => {
+      render(<SEO title="Custom title" />)
+      const { metaTags } = Helmet.peek()
+      expect(metaTags[11].content).toBe("summary")
+    })
+
+    it("twitter:card (image)", () => {
+      render(
+        <SEO
+          title="Custom title"
+          image={{
+            src: "/slug-to-image",
+            alt: "Alt text",
+            width: 1200,
+            height: 628,
+          }}
+        />
+      )
+      const { metaTags } = Helmet.peek()
+      expect(metaTags[11].content).toBe("summary_large_image")
+    })
+
+    it("twitter:image", () => {
+      render(
+        <SEO
+          title="Custom title"
+          image={{
+            src: "/slug-to-image",
+            alt: "Alt text",
+            width: 1200,
+            height: 628,
+          }}
+        />
+      )
+      const { metaTags } = Helmet.peek()
+      expect(metaTags[16].content).toBe(mockImage.src)
+    })
+
+    it("twitter:image:alt", () => {
+      render(
+        <SEO
+          title="Custom title"
+          image={{
+            src: "/slug-to-image",
+            alt: "Alt text",
+            width: 1200,
+            height: 628,
+          }}
+        />
+      )
+      const { metaTags } = Helmet.peek()
+      expect(metaTags[17].content).toBe(mockImage.alt)
+    })
   })
 })
