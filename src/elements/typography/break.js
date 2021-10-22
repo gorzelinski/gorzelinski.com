093fc341cec3537d7c02b188938a294c3b627470
gorzelinski <mateusz.gorzelinski@gmail.com>
2021-10-22 13:22:33 +0200

    refactor: display of hr element

--- src/elements/typography/break.js
+++ src/elements/typography/break.js
@@ -1,7 +1,15 @@
 import styled from "styled-components"
+import { media } from "../utils"
 
 export const Hr = styled.hr`
-  background-color: ${props => props.theme.color.surface.shade200};
+  margin: ${props => props.theme.space.xl} auto;
+  background-color: ${props => props.theme.color.primary.base};
   border: 0;
   height: ${props => props.theme.space.xxs};
+  width: ${props => props.theme.space.xxl};
+  max-width: 62%;
+
+  ${media.mobile`
+    width: ${props => props.theme.space.xxxl};
+  `}
 `
