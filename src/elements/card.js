4ea7815ea590829eb670d18c92110b712d24d672
gorzelinski <mateusz.gorzelinski@gmail.com>
2021-10-29 13:30:51 +0200

    fix: delete max width from card element

--- src/elements/card.js
+++ src/elements/card.js
@@ -2,14 +2,11 @@ import styled, { css } from "styled-components"
 
 import { full, span2, span3, span4 } from "./grid"
 
-// think about alignment - content/itself
 export const Card = styled.article`
   ${span2}
   ${span3}
   ${span4}
   ${full}
-
-  max-width: calc(${props => props.theme.space.xxxl} * 1.618 * 1.618);
 
   ${props =>
     props.$center &&
