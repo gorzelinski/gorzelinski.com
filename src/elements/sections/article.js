0cef3b070e4d0f4aaf5db0d9727ea8fc46490d13
gorzelinski <mateusz.gorzelinski@gmail.com>
2021-11-02 21:47:58 +0100

    refactor: simplify article element

--- src/elements/sections/article.js
+++ src/elements/sections/article.js
@@ -11,22 +11,16 @@ export const Article = styled.article`
     grid-column: 2;
   }
 
-  /* maybe change to whole header */
   & > figure {
     grid-column: 1 / span 3;
   }
 
   & > header {
     margin-bottom: ${props => props.theme.space.l};
-    padding-bottom: ${props => props.theme.space.xs};
-    border-bottom: ${props => props.theme.space.xxs} solid
-      ${props => props.theme.color.surface.shade200};
   }
 
   & > footer {
-    padding-top: ${props => props.theme.space.xs};
-    border-top: ${props => props.theme.space.xxs} solid
-      ${props => props.theme.color.surface.shade200};
+    margin-top: ${props => props.theme.font.height.base};
   }
 
   & > div > *:first-child {
