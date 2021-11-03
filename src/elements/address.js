bdfb5cdf633c517c777ee6886701cacf53f68893
gorzelinski <mateusz.gorzelinski@gmail.com>
2021-11-03 10:38:31 +0100

    refactor: make address element more abstract

--- src/elements/address.js
+++ src/elements/address.js
@@ -1,11 +1,5 @@
 import styled from "styled-components"
 
-// think about making it more abstract
 export const Address = styled.address`
   font-style: inherit;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: ${props => props.theme.space.s};
-  margin-bottom: ${props => props.theme.space.l};
 `
