8f616053f56895dd59eb82ef76936644c4ad643d
gorzelinski <mateusz.gorzelinski@gmail.com>
2021-11-05 18:24:14 +0100

    refactor: simplify page footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,19 +6,17 @@ import {
   Button,
   Card,
   Footer as StyledFooter,
-  H6,
-  Navigation,
   Small,
 } from "../elements"
 import Socials from "../components/socials"
 
 const Footer = () => {
   const { bio } = useBio()
-  const { title } = bio.site.siteMetadata
+  const { title, social } = bio.site.siteMetadata
   const { email } = bio.site.siteMetadata?.author
 
   const links = Object.fromEntries(
-    Object.entries(bio.site.siteMetadata?.social).map(array => [
+    Object.entries(social).map(array => [
       array[0],
       `https://${array[0]}.com/${array[1]}`,
     ])
@@ -26,9 +24,7 @@ const Footer = () => {
   return (
     <StyledFooter $border $lower id="kontakt">
       <Card as="div">
-        <H6 as="p" $top>
-          Napisz mi maila
-        </H6>
+        <Small $top>Napisz mi maila</Small>
         <Address>
           <Button $text $first as="a" href={`mailto:${email}`}>
             {email}
@@ -36,30 +32,12 @@ const Footer = () => {
         </Address>
       </Card>
       <Card as="div">
-        <H6 as="p" $top>
-          Znajdź mnie także na
-        </H6>
+        <Small $top>Znajdź mnie także na</Small>
         <Socials data={links}></Socials>
       </Card>
       <Card as="div">
-        <H6 as="p" $top>
-          Gorzeliński
-        </H6>
-        <Navigation as="div">
-          <Button $mobile $nav $flex $first to="/portfolio">
-            Portfolio
-          </Button>
-          <Button $mobile $nav $flex to="/o-mnie">
-            O mnie
-          </Button>
-          <Button $mobile $nav $flex to="/blog">
-            Blog
-          </Button>
-        </Navigation>
-      </Card>
-      <Card as="div" $full>
         <Small $top $bottom>
-          © {new Date().getFullYear()} {title} | Z miłością stworzyłem tę stronę
+          © {new Date().getFullYear()} {title} | Stworzyłem tę stronę z miłością
         </Small>
       </Card>
     </StyledFooter>
