c8fa241318b3fee655b96e34fe64d7a00206dc18
gorzelinski <mateusz.gorzelinski@gmail.com>
2021-11-06 10:17:36 +0100

    refactor: shorten a11y e2e tests

--- cypress/e2e/accessibility.test.js
+++ cypress/e2e/accessibility.test.js
@@ -1,40 +1,27 @@
 /// <reference types="Cypress" />
 
 describe("Accessibility tests", () => {
-  it("Has no detectable accessibility violations on load", () => {
-    cy.visit("/").get("main").injectAxe()
+  afterEach(() => {
     cy.checkA11y()
     cy.findByRole("button", { name: /motyw/i, exact: false }).click()
     cy.checkA11y()
+  })
+  it("Has no detectable accessibility violations on load", () => {
+    cy.visit("/").get("main").injectAxe()
   })
   it("Visits portfolio page and checks for accessibility violations", () => {
     cy.visit("/portfolio").get("main").injectAxe()
-    cy.checkA11y()
-    cy.findByRole("button", { name: /motyw/i, exact: false }).click()
-    cy.checkA11y()
   })
   it("Visits portfolio project page and checks for accessibility violations", () => {
     cy.visit("/portfolio/an-lam").get("main").injectAxe()
-    cy.checkA11y()
-    cy.findByRole("button", { name: /motyw/i, exact: false }).click()
-    cy.checkA11y()
   })
   it("Visits about me page and checks for accessibility violations", () => {
     cy.visit("/o-mnie").get("main").injectAxe()
-    cy.checkA11y()
-    cy.findByRole("button", { name: /motyw/i, exact: false }).click()
-    cy.checkA11y()
   })
   it("Visits blog page and checks for accessibility violations", () => {
     cy.visit("/blog").get("main").injectAxe()
-    cy.checkA11y()
-    cy.findByRole("button", { name: /motyw/i, exact: false }).click()
-    cy.checkA11y()
   })
   it("Visits blog post page and checks for accessibility violations", () => {
     cy.visit("/blog/hello-world").get("main").injectAxe()
-    cy.checkA11y()
-    cy.findByRole("button", { name: /motyw/i, exact: false }).click()
-    cy.checkA11y()
   })
 })
