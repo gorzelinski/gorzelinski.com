69e7703974a2e54cc8f757ffdac0245c444221a8
gorzelinski <mateusz.gorzelinski@gmail.com>
2021-11-12 17:41:16 +0100

    test: add image data mocks to post tests

--- src/components/__tests__/post.js
+++ src/components/__tests__/post.js
@@ -14,6 +14,35 @@ const defaultData = {
     date: "2021-05-01T22:12:03.284Z",
     description: "Default description",
     title: "Default title",
+    image: {
+      alt: "Default alt text",
+      src: {
+        childImageSharp: {
+          gatsbyImageData: {
+            layout: "constrained",
+            backgroundColor: "#0868c8",
+            images: {
+              fallback: {
+                src: "/static/ea672a9ab189f45e297b00a559bb3962/b8e2d/gorzelinski-thumbnail.png",
+                srcSet:
+                  "/static/ea672a9ab189f45e297b00a559bb3962/9a63f/gorzelinski-thumbnail.png 640w,\n/static/ea672a9ab189f45e297b00a559bb3962/87706/gorzelinski-thumbnail.png 1280w,\n/static/ea672a9ab189f45e297b00a559bb3962/b8e2d/gorzelinski-thumbnail.png 2560w",
+                sizes: "(min-width: 2560px) 2560px, 100vw",
+              },
+              sources: [
+                {
+                  srcSet:
+                    "/static/ea672a9ab189f45e297b00a559bb3962/17574/gorzelinski-thumbnail.webp 640w,\n/static/ea672a9ab189f45e297b00a559bb3962/71d4d/gorzelinski-thumbnail.webp 1280w,\n/static/ea672a9ab189f45e297b00a559bb3962/c87c1/gorzelinski-thumbnail.webp 2560w",
+                  type: "image/webp",
+                  sizes: "(min-width: 2560px) 2560px, 100vw",
+                },
+              ],
+            },
+            width: 2560,
+            height: 1440,
+          },
+        },
+      },
+    },
   },
   timeToRead: 5,
 }
--- src/components/__tests__/posts.js
+++ src/components/__tests__/posts.js
@@ -17,6 +17,35 @@ const defaultData = {
           date: "01 lipca, 2021",
           title: "Default title",
           description: "Default description",
+          image: {
+            alt: "Default alt text",
+            src: {
+              childImageSharp: {
+                gatsbyImageData: {
+                  layout: "constrained",
+                  backgroundColor: "#0868c8",
+                  images: {
+                    fallback: {
+                      src: "/static/ea672a9ab189f45e297b00a559bb3962/b8e2d/gorzelinski-thumbnail.png",
+                      srcSet:
+                        "/static/ea672a9ab189f45e297b00a559bb3962/9a63f/gorzelinski-thumbnail.png 640w,\n/static/ea672a9ab189f45e297b00a559bb3962/87706/gorzelinski-thumbnail.png 1280w,\n/static/ea672a9ab189f45e297b00a559bb3962/b8e2d/gorzelinski-thumbnail.png 2560w",
+                      sizes: "(min-width: 2560px) 2560px, 100vw",
+                    },
+                    sources: [
+                      {
+                        srcSet:
+                          "/static/ea672a9ab189f45e297b00a559bb3962/17574/gorzelinski-thumbnail.webp 640w,\n/static/ea672a9ab189f45e297b00a559bb3962/71d4d/gorzelinski-thumbnail.webp 1280w,\n/static/ea672a9ab189f45e297b00a559bb3962/c87c1/gorzelinski-thumbnail.webp 2560w",
+                        type: "image/webp",
+                        sizes: "(min-width: 2560px) 2560px, 100vw",
+                      },
+                    ],
+                  },
+                  width: 2560,
+                  height: 1440,
+                },
+              },
+            },
+          },
         },
         timeToRead: 4,
       },
@@ -29,6 +58,35 @@ const defaultData = {
           date: "28 maja, 2021",
           title: "Default title 2",
           description: "Default description 2",
+          image: {
+            alt: "Default alt text",
+            src: {
+              childImageSharp: {
+                gatsbyImageData: {
+                  layout: "constrained",
+                  backgroundColor: "#0868c8",
+                  images: {
+                    fallback: {
+                      src: "/static/ea672a9ab189f45e297b00a559bb3962/b8e2d/gorzelinski-thumbnail.png",
+                      srcSet:
+                        "/static/ea672a9ab189f45e297b00a559bb3962/9a63f/gorzelinski-thumbnail.png 640w,\n/static/ea672a9ab189f45e297b00a559bb3962/87706/gorzelinski-thumbnail.png 1280w,\n/static/ea672a9ab189f45e297b00a559bb3962/b8e2d/gorzelinski-thumbnail.png 2560w",
+                      sizes: "(min-width: 2560px) 2560px, 100vw",
+                    },
+                    sources: [
+                      {
+                        srcSet:
+                          "/static/ea672a9ab189f45e297b00a559bb3962/17574/gorzelinski-thumbnail.webp 640w,\n/static/ea672a9ab189f45e297b00a559bb3962/71d4d/gorzelinski-thumbnail.webp 1280w,\n/static/ea672a9ab189f45e297b00a559bb3962/c87c1/gorzelinski-thumbnail.webp 2560w",
+                        type: "image/webp",
+                        sizes: "(min-width: 2560px) 2560px, 100vw",
+                      },
+                    ],
+                  },
+                  width: 2560,
+                  height: 1440,
+                },
+              },
+            },
+          },
         },
         timeToRead: 4,
       },
@@ -41,6 +99,35 @@ const defaultData = {
           date: "06 maja, 2021",
           title: "Default title 3",
           description: "Default description 3",
+          image: {
+            alt: "Default alt text",
+            src: {
+              childImageSharp: {
+                gatsbyImageData: {
+                  layout: "constrained",
+                  backgroundColor: "#0868c8",
+                  images: {
+                    fallback: {
+                      src: "/static/ea672a9ab189f45e297b00a559bb3962/b8e2d/gorzelinski-thumbnail.png",
+                      srcSet:
+                        "/static/ea672a9ab189f45e297b00a559bb3962/9a63f/gorzelinski-thumbnail.png 640w,\n/static/ea672a9ab189f45e297b00a559bb3962/87706/gorzelinski-thumbnail.png 1280w,\n/static/ea672a9ab189f45e297b00a559bb3962/b8e2d/gorzelinski-thumbnail.png 2560w",
+                      sizes: "(min-width: 2560px) 2560px, 100vw",
+                    },
+                    sources: [
+                      {
+                        srcSet:
+                          "/static/ea672a9ab189f45e297b00a559bb3962/17574/gorzelinski-thumbnail.webp 640w,\n/static/ea672a9ab189f45e297b00a559bb3962/71d4d/gorzelinski-thumbnail.webp 1280w,\n/static/ea672a9ab189f45e297b00a559bb3962/c87c1/gorzelinski-thumbnail.webp 2560w",
+                        type: "image/webp",
+                        sizes: "(min-width: 2560px) 2560px, 100vw",
+                      },
+                    ],
+                  },
+                  width: 2560,
+                  height: 1440,
+                },
+              },
+            },
+          },
         },
         timeToRead: 1,
       },
@@ -53,6 +140,35 @@ const defaultData = {
           date: "01 maja, 2021",
           title: "Default title 4",
           description: "Default description 4",
+          image: {
+            alt: "Default alt text",
+            src: {
+              childImageSharp: {
+                gatsbyImageData: {
+                  layout: "constrained",
+                  backgroundColor: "#0868c8",
+                  images: {
+                    fallback: {
+                      src: "/static/ea672a9ab189f45e297b00a559bb3962/b8e2d/gorzelinski-thumbnail.png",
+                      srcSet:
+                        "/static/ea672a9ab189f45e297b00a559bb3962/9a63f/gorzelinski-thumbnail.png 640w,\n/static/ea672a9ab189f45e297b00a559bb3962/87706/gorzelinski-thumbnail.png 1280w,\n/static/ea672a9ab189f45e297b00a559bb3962/b8e2d/gorzelinski-thumbnail.png 2560w",
+                      sizes: "(min-width: 2560px) 2560px, 100vw",
+                    },
+                    sources: [
+                      {
+                        srcSet:
+                          "/static/ea672a9ab189f45e297b00a559bb3962/17574/gorzelinski-thumbnail.webp 640w,\n/static/ea672a9ab189f45e297b00a559bb3962/71d4d/gorzelinski-thumbnail.webp 1280w,\n/static/ea672a9ab189f45e297b00a559bb3962/c87c1/gorzelinski-thumbnail.webp 2560w",
+                        type: "image/webp",
+                        sizes: "(min-width: 2560px) 2560px, 100vw",
+                      },
+                    ],
+                  },
+                  width: 2560,
+                  height: 1440,
+                },
+              },
+            },
+          },
         },
         timeToRead: 3,
       },
