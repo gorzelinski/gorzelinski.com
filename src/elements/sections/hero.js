faee2ccc8bde7f3b7ccce8176d930315b8c5c787
gorzelinski <mateusz.gorzelinski@gmail.com>
2021-11-12 10:24:57 +0100

    feat: add mobile styles for hero section

--- src/elements/sections/hero.js
+++ src/elements/sections/hero.js
@@ -1,9 +1,35 @@
 import styled from "styled-components"
 
-import { Section } from "./section"
+import { grid } from "../grid"
+import { media } from "../utils"
 
-export const Hero = styled(Section)`
+export const Hero = styled.section`
+  ${grid}
+  padding-top: ${props => props.theme.space.xxl};
+
+  @media (orientation: portrait) {
+    --mobileNavHeight: calc(
+      4 * ${props => props.theme.space.xs} +
+        ${props => props.theme.font.height.tiny}
+    );
+    --marginTop: ${props => props.theme.space.xl};
+
+    padding-top: ${props => props.theme.space.m};
+    min-height: calc(100vh - var(--mobileNavHeight) - var(--marginTop));
+    align-items: center;
+  }
+
+  ${media.mobile`
+    @media (orientation: portrait) {
+      padding-top: ${props => props.theme.space.xxl};
+      min-height: auto;
+      align-items: auto;
+    }
+    min-height: auto;
+    align-items: start;
+  `}
+
   & > * {
-    max-width: calc(${props => props.theme.space.xxxl} * 1.618 * 1.618);
+    max-width: calc(72rem / 1.618);
   }
 `
