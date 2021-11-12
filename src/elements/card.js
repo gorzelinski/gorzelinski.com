a26b812b5ab377c6539db1128e0f378ea375007e
gorzelinski <mateusz.gorzelinski@gmail.com>
2021-11-12 13:14:22 +0100

    feat: add styling to card element

--- src/elements/card.js
+++ src/elements/card.js
@@ -1,22 +1,52 @@
 import styled, { css } from "styled-components"
 
-import { full, span2, span3, span4 } from "./grid"
+import { media } from "./utils"
+import { spanAll, span2, span3, span4 } from "./grid"
 
 export const Card = styled.article`
   ${span2}
   ${span3}
   ${span4}
-  ${full}
+  ${spanAll}
+  overflow: hidden;
+  display: grid;
+  align-content: start;
+  gap: ${props => props.theme.space.s};
+  border: 1px solid ${props => props.theme.color.surface.shade300};
+  border-radius: ${props => props.theme.space.s};
+
+  & > div:nth-child(2) {
+    padding: calc(${props => props.theme.space.s} - 1px);
+  }
 
   ${props =>
-    props.$center &&
+    props.$horizontal &&
     css`
-      justify-self: center;
+      grid-template-columns: repeat(
+        auto-fit,
+        minmax(min(${props => props.theme.space.xxxl}, 100%), 1fr)
+      );
+      min-height: ${props => props.theme.space.xxxl};
+      align-content: stretch;
+
+      & > *:first-child {
+        max-width: 100%;
+        align-self: start;
+        /* height: 100%; */
+      }
+
+      ${media.desktop`
+        & > *:first-child {
+          height: 100%;
+        }
+        & > div:nth-child(2) {
+          grid-column: span 2;
+        }
+      `}
     `}
 
-  ${props =>
-    props.$textCenter &&
-    css`
-      text-align: center;
-    `}
+  &:hover {
+    box-shadow: ${props => props.theme.shadow.neumorphism};
+    transition: box-shadow ${props => props.theme.duration.immediate} ease-in;
+  }
 `
