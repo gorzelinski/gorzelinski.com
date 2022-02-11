7fd981eefd17f9a918a34af665b5b7453bab10b5
gorzelinski <mateusz.gorzelinski@gmail.com>
2022-02-11 12:41:06 +0100

    test(unit): fix navbar test

--- src/components/__tests__/navbar.js
+++ src/components/__tests__/navbar.js
@@ -31,24 +31,22 @@ describe("Navbar component", () => {
 
     it("link to portfolio page", () => {
       const portfolio = screen.getByRole("link", { name: /portfolio/i })
-      expect(portfolio.getAttribute("href")).toBe("/portfolio")
+      expect(portfolio.getAttribute("href")).toBe("/portfolio/")
+    })
+
+    it("link to about page", () => {
+      const about = screen.queryByRole("link", { name: /about/i })
+      expect(about.getAttribute("href")).toBe("/about")
+    })
+
+    it("link to blog page", () => {
+      const blog = screen.queryByRole("link", { name: /blog/i })
+      expect(blog.getAttribute("href")).toBe("/blog/")
     })
 
     it("link to contact section", () => {
       const contact = screen.getByRole("link", { name: /contact/i })
       expect(contact.getAttribute("href")).toBe("#contact")
-    })
-  })
-
-  describe("doesn't render", () => {
-    it("link to about page (default on mobile)", () => {
-      const about = screen.queryByRole("link", { name: /about/i })
-      expect(about).not.toBeInTheDocument()
-    })
-
-    it("link to blog page (default on mobile)", () => {
-      const blog = screen.queryByRole("link", { name: /blog/i })
-      expect(blog).not.toBeInTheDocument()
     })
   })
 })
