7b9158bd0ffbead85a0bab33a5d0846025163ec4
gorzelinski <mateusz.gorzelinski@gmail.com>
2022-05-05 19:44:39 +0200

    feat(page): add new sections to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,10 +4,11 @@ import { graphql } from "gatsby"
 import { getImage } from "gatsby-plugin-image"
 import { useLocalization } from "gatsby-theme-i18n"
 
-import { H1, H2, H6, Image, P, Section, Tile } from "../elements"
+import { H1, H2, H3, H6, Image, Li, P, Section, Tile, Ul } from "../elements"
 import { createMetaImage } from "../utils"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Link from "../components/link"
 
 const About = ({ data, location }) => {
   const { locale } = useLocalization()
@@ -58,7 +59,7 @@ const About = ({ data, location }) => {
       </Section>
       <Section>
         <Tile $span="all">
-          <H2 $marginReset="both">{t("weird")}</H2>
+          <H2 $marginReset="both">{t("random")}</H2>
         </Tile>
         {t("facts", { returnObjects: true }).map((fact, index) => (
           <Tile key={`fact-${index + 1}`}>
@@ -66,6 +67,44 @@ const About = ({ data, location }) => {
               {fact.heading}
             </H6>
             <P $marginReset="both">{fact.description}</P>
+          </Tile>
+        ))}
+      </Section>
+      <Section>
+        <Tile $span="all">
+          <H2 $marginReset="top">{t("core.heading")}</H2>
+          <P $type="lead" $marginReset="bottom">
+            {t("core.description")}
+          </P>
+        </Tile>
+        {t("values", { returnObjects: true }).map(value => (
+          <Tile $span={2} key={value.heading}>
+            <H3 $marginReset="top">{value.heading}</H3>
+            <P $type="lead" $marginReset="bottom">
+              {value.description}
+            </P>
+          </Tile>
+        ))}
+      </Section>
+      <Section>
+        <Tile $span="all">
+          <H2 $marginReset="top">{t("cultural.heading")}</H2>
+          <P $type="lead" $marginReset="bottom">
+            {t("cultural.description")}
+          </P>
+        </Tile>
+        {t("corner", { returnObjects: true }).map((media, index) => (
+          <Tile key={`media-${index}`}>
+            <H6 $marginReset="top" as="h3">
+              {media.heading}
+            </H6>
+            <Ul $marginReset="both">
+              {media.items.map((item, index) => (
+                <Li key={`title-${index}`}>
+                  <Link href={item.link}>{item.title}</Link>
+                </Li>
+              ))}
+            </Ul>
           </Tile>
         ))}
       </Section>
