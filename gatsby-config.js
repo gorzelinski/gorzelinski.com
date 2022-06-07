bb876ba617a1986da5dcdbb437bd8cd78ec4fa83
gorzelinski <mateusz.gorzelinski@gmail.com>
2022-06-07 19:22:57 +0200

    build: modify config to generate valid rss feeds in both languages

--- gatsby-config.js
+++ gatsby-config.js
@@ -101,31 +101,31 @@ module.exports = {
         locales: `./content/i18n/react-i18next`,
         i18nextOptions: {
           ns: [
-            "pages/404",
-            "pages/about",
-            "pages/blog",
-            "pages/index",
-            "pages/preferences",
-            "pages/portfolio",
-            "pages/thank-you",
-            "pages/uses",
-            "templates/blog-post",
-            "templates/portfolio-project",
-            "components/bio",
-            "components/cards",
-            "components/contact",
-            "components/core-values",
-            "components/cultural-corner",
-            "components/footer",
-            "components/landing",
-            "components/my-tech",
-            "components/navbar",
-            "components/pagination",
-            "components/post",
-            "components/project",
-            "components/random-facts",
-            "components/subscribe",
-            "components/theme-switcher",
+            `pages/404`,
+            `pages/about`,
+            `pages/blog`,
+            `pages/index`,
+            `pages/preferences`,
+            `pages/portfolio`,
+            `pages/thank-you`,
+            `pages/uses`,
+            `templates/blog-post`,
+            `templates/portfolio-project`,
+            `components/bio`,
+            `components/cards`,
+            `components/contact`,
+            `components/core-values`,
+            `components/cultural-corner`,
+            `components/footer`,
+            `components/landing`,
+            `components/my-tech`,
+            `components/navbar`,
+            `components/pagination`,
+            `components/post`,
+            `components/project`,
+            `components/random-facts`,
+            `components/subscribe`,
+            `components/theme-switcher`,
           ],
         },
       },
@@ -139,52 +139,94 @@ module.exports = {
     {
       resolve: `gatsby-plugin-feed`,
       options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-                site_url: siteUrl
-              }
-            }
-          }
-        `,
         feeds: [
           {
-            serialize: ({ query: { site, allMdx } }) => {
+            serialize: ({ query: { allMdx } }) => {
               return allMdx.nodes.map(node => {
+                const url = `https://gorzelinski.com/${
+                  node.frontmatter.client ? "portfolio" : "blog"
+                }${node.fields.slug}`
                 return Object.assign({}, node.frontmatter, {
-                  description: node.excerpt,
+                  author: `Matthew Gorzelinski`,
+                  description: node.frontmatter.description,
                   date: node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + node.fields.slug,
-                  custom_elements: [{ "content:encoded": node.html }],
+                  url,
+                  guid: url,
                 })
               })
             },
             query: `
-              {
-                allMdx(
-                  sort: { order: DESC, fields: [frontmatter___date] },
-                ) {
-                  nodes {
-                    excerpt
-                    html
-                    fields {
-                      slug
-                    }
-                    frontmatter {
-                      title
-                      date
-                    }
+            {
+              allMdx(
+                sort: {order: DESC, fields: [frontmatter___date]}
+                filter: {fields: {locale: {eq: "en"}}}
+              ) {
+                nodes {
+                  excerpt
+                  fields {
+                    slug
+                  }
+                  frontmatter {
+                    title
+                    date
+                    description
+                    client
                   }
                 }
               }
+            }            
             `,
-            output: "/rss.xml",
-            title: "Gorzeliński RSS Feed",
+            output: `/rss.xml`,
+            title: `Matthew Gorzelinski's Blog`,
+            description: `I design, code, write - for humans and machines. I help by creating things on the Internet.`,
+            language: `en`,
+            site_url: `https://gorzelinski.com`,
+            feed_url: `https://gorzelinski.com/rss.xml`,
+            image_url: `https://gorzelinski.com/logo.png`,
+          },
+          {
+            serialize: ({ query: { allMdx } }) => {
+              return allMdx.nodes.map(node => {
+                const url = `https://gorzelinski.com/pl/${
+                  node.frontmatter.client ? "portfolio" : "blog"
+                }${node.fields.slug}`
+                return Object.assign({}, node.frontmatter, {
+                  author: `Mateusz Gorzeliński`,
+                  description: node.frontmatter.description,
+                  date: node.frontmatter.date,
+                  url,
+                  guid: url,
+                })
+              })
+            },
+            query: `
+            {
+              allMdx(
+                sort: {order: DESC, fields: [frontmatter___date]}
+                filter: {fields: {locale: {eq: "pl"}}}
+              ) {
+                nodes {
+                  excerpt
+                  fields {
+                    slug
+                  }
+                  frontmatter {
+                    title
+                    date
+                    description
+                    client
+                  }
+                }
+              }
+            }            
+            `,
+            output: `/pl/rss.xml`,
+            title: `Blog Mateusza Gorzelińskiego`,
+            description: `Projektuję, koduję, piszę - dla ludzi i maszyn. Pomagam tworząc rzeczy w Internecie.`,
+            language: `pl`,
+            site_url: `https://gorzelinski.com/pl/`,
+            feed_url: `https://gorzelinski.com/pl/rss.xml`,
+            image_url: `https://gorzelinski.com/logo.png`,
           },
         ],
       },
