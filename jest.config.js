7054c93816297ad47a0f99be1aa4cb850eb7208d
gorzelinski <mateusz.gorzelinski@gmail.com>
2022-06-14 14:29:15 +0200

    chore: bump up jest - major version

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,7 @@ module.exports = {
   moduleNameMapper: {
     ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/tests/__mocks__/file-mock.js`,
-    "^gatsby-page-utils/(.*)$": `gatsby-page-utils/dist/$1`, // Workaround for https://github.com/facebook/jest/issues/9771
+    "^gatsby-page-utils/(.*)$": "gatsby-page-utils/$1",
   },
   testPathIgnorePatterns: [
     `node_modules`,
@@ -20,6 +20,8 @@ module.exports = {
   globals: {
     __PATH_PREFIX__: ``,
   },
-  testURL: `http://localhost`,
+  testEnvironmentOptions: {
+    url: `http://localhost`,
+  },
   setupFilesAfterEnv: [`<rootDir>/tests/setup-test-env.js`],
 }
