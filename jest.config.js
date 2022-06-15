7bed5b2168c2837482da243d57657e256387e0bb
gorzelinski <mateusz.gorzelinski@gmail.com>
2022-06-15 11:19:03 +0200

    fix: config for unit tests

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ module.exports = {
     ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/tests/__mocks__/file-mock.js`,
     "^gatsby-page-utils/(.*)$": "gatsby-page-utils/$1",
+    "^@reach/router(.*)": "<rootDir>/node_modules/@gatsbyjs/reach-router$1",
   },
   testPathIgnorePatterns: [
     `node_modules`,
