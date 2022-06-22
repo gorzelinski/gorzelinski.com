a37c187a4bc9db8808cf10672019b53776f635bc
gorzelinski <mateusz.gorzelinski@gmail.com>
2022-06-22 10:09:57 +0200

    chore: delete unused plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -321,8 +321,5 @@ module.exports = {
     },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
