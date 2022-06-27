1ac1f583bfdc39e6539b188d71f4e0194a3eb2ca
gorzelinski <mateusz.gorzelinski@gmail.com>
2022-06-27 14:36:06 +0200

    test(e2e): refactor and improve theme test

--- cypress/e2e/theme.test.js
+++ cypress/e2e/theme.test.js
@@ -1,46 +1,44 @@
 /// <reference types="Cypress" />
 
+const light = {
+  backgroundColor: "rgb(255, 255, 255)",
+  text: "rgb(0, 0, 0)",
+}
+
+const dark = {
+  backgroundColor: "rgb(14, 15, 16)",
+  text: "rgb(255, 255, 255)",
+}
+
 describe("Theme tests", () => {
   const checkLightTheme = () => {
-    cy.findByTestId("sun-and-moon")
-      .scrollIntoView({
-        easing: "linear",
-        duration: 300,
-        offset: { top: -150 },
-      })
-      .should("be.visible")
+    cy.findByTestId("sun-and-moon").should("exist")
     cy.findByTestId("background").should(
       "have.css",
       "background-color",
-      "rgb(255, 255, 255)"
+      light.backgroundColor
     )
-    cy.findAllByRole("heading")
-      .first()
-      .should("have.css", "color", "rgb(0, 0, 0)")
+    cy.findAllByRole("heading").first().should("have.css", "color", light.text)
   }
   const checkDarkTheme = () => {
-    cy.findByTestId("sun-and-moon")
-      .scrollIntoView({
-        easing: "linear",
-        duration: 300,
-        offset: { top: -150 },
-      })
-      .should("be.visible")
+    cy.findByTestId("sun-and-moon").should("exist")
     cy.findByTestId("background").should(
       "have.css",
       "background-color",
-      "rgb(14, 15, 16)"
+      dark.backgroundColor
     )
-    cy.findAllByRole("heading")
-      .first()
-      .should("have.css", "color", "rgb(255, 255, 255)")
+    cy.findAllByRole("heading").first().should("have.css", "color", dark.text)
   }
-  before(() => {
-    window.localStorage.setItem("theme", JSON.stringify("light"))
-  })
+
   it("Visits home page and tests themes", () => {
-    cy.visit("/")
-    checkLightTheme()
+    cy.visit("/", {
+      onBeforeLoad(win) {
+        cy.stub(win, "matchMedia")
+          .withArgs("(prefers-color-scheme: dark)")
+          .returns({ matches: true })
+      },
+    })
+    checkDarkTheme()
     cy.findByLabelText(/change theme/i)
       .scrollIntoView({
         easing: "linear",
@@ -48,8 +46,8 @@ describe("Theme tests", () => {
         offset: { top: -150 },
       })
       .click()
-    checkDarkTheme()
+    checkLightTheme()
     cy.reload()
-    checkDarkTheme()
+    checkLightTheme()
   })
 })
