db4ea14266c9dd23328713c7ba357f5385ddab84
gorzelinski <mateusz.gorzelinski@gmail.com>
2022-07-20 11:39:21 +0200

    build: simplify page creation

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,16 +3,6 @@ const path = require(`path`)
 exports.createPages = async ({ graphql, actions, reporter }) => {
   const { createPage } = actions
 
-  const groupByLanguage = items =>
-    Object.values(
-      items.reduce((group, item) => {
-        const language = item.fields.locale
-        group[language] = group[language] ?? []
-        group[language].push(item)
-        return group
-      }, {})
-    )
-
   const createPageSlugs = (items, index) => {
     const previous = index === 0 ? null : items[index - 1].fields.slug
     const next =
@@ -23,18 +13,17 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
 
   const customCreatePages = (items, createPageConfig) => {
     const { path, component } = createPageConfig
-    const itemsByLanguage = groupByLanguage(items)
 
-    itemsByLanguage.forEach(items => {
-      items.forEach((item, index) => {
-        const { previous, next } = createPageSlugs(items, index)
-        const categories = item?.frontmatter?.categories
+    items.forEach(({ nodes }) => {
+      nodes.forEach((node, index) => {
+        const { previous, next } = createPageSlugs(nodes, index)
+        const categories = node?.frontmatter?.categories
 
         createPage({
-          path: `${path}${item.fields.slug}`,
+          path: `${path}${node.fields.slug}`,
           component,
           context: {
-            slug: item.fields.slug,
+            slug: node.fields.slug,
             previous,
             next,
             ...(categories && { categories }),
@@ -57,14 +46,15 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
           filter: { fileAbsolutePath: { regex: "/(blog)/" } }
           limit: 1000
         ) {
-          nodes {
-            id
-            fields {
-              locale
-              slug
-            }
-            frontmatter {
-              categories
+          group(field: fields___locale) {
+            nodes {
+              fields {
+                locale
+                slug
+              }
+              frontmatter {
+                categories
+              }
             }
           }
         }
@@ -80,11 +70,12 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
           filter: { fileAbsolutePath: { regex: "/(portfolio)/" } }
           limit: 1000
         ) {
-          nodes {
-            id
-            fields {
-              locale
-              slug
+          group(field: fields___locale) {
+            nodes {
+              fields {
+                locale
+                slug
+              }
             }
           }
         }
@@ -108,8 +99,8 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     return
   }
 
-  const posts = resultPosts.data.allMdx.nodes
-  const projects = resultProjects.data.allMdx.nodes
+  const posts = resultPosts.data.allMdx.group
+  const projects = resultProjects.data.allMdx.group
 
   // Create blog posts pages
   // But only if there's at least one markdown file found at "content/blog" (defined in gatsby-config.js)
