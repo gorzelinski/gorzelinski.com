db72bd01aca97c331b21943c0b3027e0f4f73cf5
gorzelinski <mateusz.gorzelinski@gmail.com>
2022-09-13 19:56:33 +0200

    chore: delete unnecessary comments

--- gatsby-node.js
+++ gatsby-node.js
@@ -33,11 +33,9 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
     })
   }
 
-  // Define a template for blog post
   const blogPost = path.resolve(`./src/templates/blog-post.js`)
   const portfolioProject = path.resolve(`./src/templates/portfolio-project.js`)
 
-  // Get all markdown blog posts sorted by date
   const resultPosts = await graphql(
     `
       {
@@ -102,10 +100,6 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
   const posts = resultPosts.data.allMdx.group
   const projects = resultProjects.data.allMdx.group
 
-  // Create blog posts pages
-  // But only if there's at least one markdown file found at "content/blog" (defined in gatsby-config.js)
-  // `context` is available in the template as a prop and as a variable in GraphQL
-
   if (posts.length > 0) {
     customCreatePages(posts, {
       path: "blog",
@@ -138,12 +132,6 @@ exports.onCreateNode = ({ node, actions }) => {
 exports.createSchemaCustomization = ({ actions }) => {
   const { createTypes } = actions
 
-  // Explicitly define the siteMetadata {} object
-  // This way those will always be defined even if removed from gatsby-config.js
-
-  // Also explicitly define the Markdown frontmatter
-  // This way the "MarkdownRemark" queries will return `null` even when no
-  // blog posts are stored inside "content/blog" instead of returning an error
   createTypes(`
     type Social {
       github: String
