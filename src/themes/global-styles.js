e7e84c2c249e52eb0b611db084ae041a5f59e706
gorzelinski <mateusz.gorzelinski@gmail.com>
2022-09-24 17:18:36 +0200

    feat: add scrollbar styles

--- src/themes/global-styles.js
+++ src/themes/global-styles.js
@@ -6,6 +6,13 @@ import "./light.css"
 import "./dark.css"
 
 const GlobalStyles = createGlobalStyle`
+  :root {
+    --scrollbar-color: var(--color-gray-80);
+    --scrollbar-color-thumb: var(--color-gray-70);
+    --scrollbar-border-radius: var(--border-radius-00);
+    --scrollbar-width: var(--space-30);
+    --scrollbar-height: var(--space-20);
+  }
   *,
   :after,
   :before {
@@ -14,6 +21,21 @@ const GlobalStyles = createGlobalStyle`
   ::selection {
     background-color: var(--color-primary-90);
     color: var(--color-primary-40);
+  }
+  ::-webkit-scrollbar {
+    background-color: var(--scrollbar-color);
+    width: var(--scrollbar-width);
+  }
+  ::-webkit-scrollbar:horizontal {
+    border-radius: var(--scrollbar-border-radius);
+    height: var(--scrollbar-height);
+  }
+  ::-webkit-scrollbar-thumb {
+    background-color: var(--scrollbar-color-thumb);
+    border-radius: var(--scrollbar-border-radius);
+    &:hover, &:active {
+      --scrollbar-color-thumb: var(--color-gray-60);
+    }
   }
 `
 
