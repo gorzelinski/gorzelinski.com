5729f04b6c3fffecba3dd70666a080a06cde38b4
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2023-12-21 16:57:42 +0100

    feat: Add transitions to the Anchor component

--- components/anchor/anchor.styles.tsx
+++ components/anchor/anchor.styles.tsx
@@ -5,22 +5,22 @@ export const anchor = cva({
     borderRadius: 's',
     cursor: 'pointer',
     color: 'primary.400',
-    textDecoration: 'underline',
+    textDecorationLine: 'underline',
+    textUnderlineOffset: 'var(--spacing-2xs)',
+    textDecorationThickness: 'auto',
     textDecorationColor: 'transparent',
-    transitionProperty: 'text-decoration-color',
-    transitionDuration: 'fast',
-    transitionTimingFunction: 'easeIn',
+    transitionProperty: 'color, text-decoration-color',
+    transitionDuration: 'natural',
+    transitionTimingFunction: 'easeOut',
     _hover: {
       textDecorationColor: 'primary.400'
     },
     _focusVisible: {
-      textDecorationColor: 'transparent',
-      outline: 'primary.regular',
-      outlineOffset: '2xs'
+      textDecorationColor: 'primary.400',
+      outline: 'none'
     },
     _visited: {
-      color: 'primary.300',
-      textDecorationColor: 'primary.300'
+      color: 'primary.400'
     },
     '& > :is(h1, h2, h3, h4, p, small, li, caption, figcaption, code)': {
       color: 'primary.400'
