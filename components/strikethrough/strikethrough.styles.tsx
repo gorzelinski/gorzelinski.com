8a769b71c9065220c95ddfb04616870c68556dbb
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2023-12-22 11:27:24 +0100

    feat: Add transitions to the Strikethrough component

--- components/strikethrough/strikethrough.styles.tsx
+++ components/strikethrough/strikethrough.styles.tsx
@@ -1,8 +1,11 @@
 import { cva } from '@/styled-system/css'
+import { sharedTransitionProperties } from '../utils'
 
 export const strikethrough = cva({
   base: {
     textDecoration: 'line-through',
-    color: 'gray.300'
+    color: 'gray.300',
+    transitionProperty: 'color',
+    ...sharedTransitionProperties
   }
 })
