cd48ff60849ec005a85ab8748177ffb8f806ea05
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2023-12-22 10:38:21 +0100

    feat: Add transitions to the Italic component

--- components/italic/italic.styles.tsx
+++ components/italic/italic.styles.tsx
@@ -1,8 +1,11 @@
 import { cva } from '@/styled-system/css'
+import { sharedTransitionProperties } from '../utils'
 
 export const italic = cva({
   base: {
     color: 'gray.300',
-    fontStyle: 'italic'
+    fontStyle: 'italic',
+    transitionProperty: 'color',
+    ...sharedTransitionProperties
   }
 })
