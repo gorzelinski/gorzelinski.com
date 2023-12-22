8996758135e28bbf7e6811efff58f1b1c5c5db50
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2023-12-22 11:55:51 +0100

    feat: Add transitions to the Thematic break component

--- components/thematic-break/thematic-break.styles.tsx
+++ components/thematic-break/thematic-break.styles.tsx
@@ -1,11 +1,15 @@
 import { cva } from '@/styled-system/css'
+import { sharedTransitionProperties } from '../utils'
 
 export const thematicBreak = cva({
   base: {
     backgroundColor: 'primary.400',
-    height: '2px',
+    border: 'none',
+    height: 'var(--spacing-xs)',
     width: 'm',
     maxWidth: 'calc(100% / 1.618)',
-    marginX: 'auto'
+    marginX: 'auto',
+    transitionProperty: 'background-color',
+    ...sharedTransitionProperties
   }
 })
