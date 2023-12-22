23e2e33e1f60f6e70008eaa821fa9b489e9f4434
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2023-12-22 11:59:50 +0100

    feat: Add transitions to the Typewriter component

--- components/typewriter/typewriter.styles.tsx
+++ components/typewriter/typewriter.styles.tsx
@@ -1,11 +1,14 @@
 import { cva } from '@/styled-system/css'
+import { sharedTransitionProperties } from '../utils'
 
 export const typewriter = cva({
   base: {
     _after: {
       content: '"|"',
       animation: 'blinking',
-      color: 'primary.400'
+      color: 'primary.400',
+      transitionProperty: 'color',
+      ...sharedTransitionProperties
     }
   }
 })
