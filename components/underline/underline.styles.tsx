a8c4234915855fd8fe4b8898db0f90402402ac4e
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2023-12-22 12:05:25 +0100

    feat: Add transitions to the Underline component

--- components/underline/underline.styles.tsx
+++ components/underline/underline.styles.tsx
@@ -1,7 +1,11 @@
 import { cva } from '@/styled-system/css'
+import { sharedTransitionProperties } from '../utils'
 
 export const underline = cva({
   base: {
-    textDecoration: 'underline'
+    textDecorationLine: 'underline',
+    textUnderlineOffset: 'var(--spacing-2xs)',
+    transitionProperty: 'text-decoration-color',
+    ...sharedTransitionProperties
   }
 })
