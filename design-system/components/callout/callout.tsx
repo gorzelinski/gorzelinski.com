4355c4f649a97aa00312f6fca8f65787a66b03c3
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2024-01-09 18:50:40 +0100

    refactor(callout): Replace JSX element with a function

--- design-system/components/callout/callout.tsx
+++ design-system/components/callout/callout.tsx
@@ -1,4 +1,3 @@
-import { styled } from '@/styled-system/jsx'
 import { callout } from './callout.styles'
 import { CalloutIcons, CalloutProps } from './callout.types'
 import {
@@ -15,17 +14,15 @@ const calloutIcon: CalloutIcons = {
   success: <CheckmarkCircle size="l" className="icon"></CheckmarkCircle>
 }
 
-const StyledCallout = styled('div', callout)
-
 export const Callout = (props: CalloutProps) => {
   const { children, ...calloutVariantProps } = props
 
   return (
-    <StyledCallout {...calloutVariantProps}>
+    <div className={callout(calloutVariantProps)}>
       {calloutVariantProps.style
         ? calloutIcon[calloutVariantProps.style]
         : calloutIcon.info}
       {children}
-    </StyledCallout>
+    </div>
   )
 }
