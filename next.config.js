aaec20bac0ae07f121ab4545236149ac523014d5
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2024-02-21 19:02:08 +0100

    feat: Add basic MDX support.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+const withMDX = require('@next/mdx')()
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   trailingSlash: true
+  // Optionally, add any other Next.js config below
 }
 
-module.exports = nextConfig
+module.exports = withMDX(nextConfig)
