15c1fbcf4f732fae21b9e758b4233eb745f7616f
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2024-02-24 17:23:45 +0100

    Revert "feat: Add basic MDX support."
    This reverts commit aaec20bac0ae07f121ab4545236149ac523014d5.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-const withMDX = require('@next/mdx')()
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure `pageExtensions` to include MDX files
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   trailingSlash: true
-  // Optionally, add any other Next.js config below
 }
 
-module.exports = withMDX(nextConfig)
+module.exports = nextConfig
