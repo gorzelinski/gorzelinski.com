2147b1a19b98b695e9aadfc26144e435c0bd4268
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2024-03-08 19:40:52 +0100

    fix: Middleware matcher for images

--- middleware.ts
+++ middleware.ts
@@ -62,5 +62,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: '/((?!api|_next/static|_next/image|img/|favicon.ico).*)'
+  matcher: '/((?!api|_next/static|_next/images|image|img/|favicon.ico).*)'
 }
