92aea145eafaa36fd6944a51ad10a6ad4c99b05e
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2024-03-20 18:48:31 +0100

    feat: Add more options to the vertical rhythm util

--- design-system/utils/spacing.tsx
+++ design-system/utils/spacing.tsx
@@ -11,12 +11,19 @@ export const verticalRhythm = {
     })
   },
   marginBottom: {
+    l: css.raw({
+      marginBottom: {
+        base: 'verticalRhythm.l.base',
+        md: 'verticalRhythm.l.md',
+        lg: 'verticalRhythm.l.lg',
+        '2xl': 'verticalRhythm.l.2xl'
+      }
+    }),
     m: css.raw({
       marginBottom: {
         base: 'verticalRhythm.m.base',
         md: 'verticalRhythm.m.md',
         lg: 'verticalRhythm.m.lg',
-        xl: 'verticalRhythm.m.xl',
         '2xl': 'verticalRhythm.m.2xl'
       }
     }),
@@ -25,19 +32,41 @@ export const verticalRhythm = {
         base: 'verticalRhythm.s.base',
         md: 'verticalRhythm.s.md',
         lg: 'verticalRhythm.s.lg',
-        xl: 'verticalRhythm.s.xl',
         '2xl': 'verticalRhythm.s.2xl'
       }
     })
   },
   marginTop: {
-    m: css.raw({
+    '2xmarginBottom': css.raw({
       marginTop: {
         base: 'calc(2 * var(--spacing-vertical-rhythm-m-base))',
         md: 'calc(2 * var(--spacing-vertical-rhythm-m-md))',
         lg: 'calc(2 * var(--spacing-vertical-rhythm-m-lg))',
-        xl: 'calc(2 * var(--spacing-vertical-rhythm-m-xl))',
         '2xl': 'calc(2 * var(--spacing-vertical-rhythm-m-2xl))'
+      }
+    }),
+    l: css.raw({
+      marginTop: {
+        base: 'verticalRhythm.l.base',
+        md: 'verticalRhythm.l.md',
+        lg: 'verticalRhythm.l.lg',
+        '2xl': 'verticalRhythm.l.2xl'
+      }
+    }),
+    m: css.raw({
+      marginTop: {
+        base: 'verticalRhythm.m.base',
+        md: 'verticalRhythm.m.md',
+        lg: 'verticalRhythm.m.lg',
+        '2xl': 'verticalRhythm.m.2xl'
+      }
+    }),
+    s: css.raw({
+      marginTop: {
+        base: 'verticalRhythm.s.base',
+        md: 'verticalRhythm.s.md',
+        lg: 'verticalRhythm.s.lg',
+        '2xl': 'verticalRhythm.s.2xl'
       }
     })
   }
