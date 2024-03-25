511529d3c47f9c7d8d29578a5be28dae6c555247
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2024-03-25 17:54:49 +0100

    fix: Table row styles

--- design-system/elements/table/table-body.styles.tsx
+++ design-system/elements/table/table-body.styles.tsx
@@ -5,8 +5,10 @@ export const tableBody = cva({
   base: {
     borderTop: 'gray.regular',
     borderBottom: 'gray.regular',
-    '& tr:nth-child(2)': {
-      backgroundColor: 'primary.900'
+    '& tr': {
+      _even: {
+        backgroundColor: 'primary.900'
+      }
     },
     transitionProperty: 'border-color',
     ...sharedTransitionProperties
