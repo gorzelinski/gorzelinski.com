08e554711f6e2f9666615bd8ea444b8c21f58dd5
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2024-05-01 13:38:10 +0200

    test: Add tests for the LanguageSwitch component

---
+++ design-system/components/language-switch/__tests__/language-switch.test.tsx
@@ -0,0 +1,58 @@
+import { render, screen } from '@testing-library/react'
+import { i18n } from '@/i18n.config'
+import { LanguageSwitch } from '../language-switch'
+
+jest.mock('next/navigation', () => ({
+  usePathname: () => '/about'
+}))
+
+describe('LanguageSwitch', () => {
+  it('renders the correct number of buttons', () => {
+    render(<LanguageSwitch lang="en" />)
+
+    const buttons = screen.getAllByRole('link')
+
+    expect(buttons).toHaveLength(2)
+  })
+
+  it('renders the correct button text', () => {
+    render(<LanguageSwitch lang="en" />)
+
+    const languages = ['English', 'Polski']
+    const buttons = screen.getAllByRole('link')
+
+    buttons.forEach((button, index) => {
+      expect(button).toHaveTextContent(languages[index])
+    })
+  })
+
+  it('renders the correct lang attribute', () => {
+    render(<LanguageSwitch lang="en" />)
+
+    const buttons = screen.getAllByRole('link')
+
+    buttons.forEach((button, index) => {
+      expect(button).toHaveAttribute('lang', i18n.locales[index])
+    })
+  })
+
+  it('renders the correct href attribute', () => {
+    render(<LanguageSwitch lang="en" />)
+
+    const englishButton = screen.getByText('English')
+    const polishButton = screen.getByText('Polski')
+
+    expect(englishButton).toHaveAttribute('href', '/about')
+    expect(polishButton).toHaveAttribute('href', '/pl/about')
+  })
+
+  it('renders the correct hrefLang attribute', () => {
+    render(<LanguageSwitch lang="en" />)
+
+    const buttons = screen.getAllByRole('link')
+
+    buttons.forEach((button, index) => {
+      expect(button).toHaveAttribute('hrefLang', i18n.locales[index])
+    })
+  })
+})
