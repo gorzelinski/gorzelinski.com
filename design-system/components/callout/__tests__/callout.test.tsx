198c25d64c7dfc1fd61377cbdd2492a05f8bbd49
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2024-05-01 12:38:27 +0200

    test: Rewrite Callout tests to use screen

--- design-system/components/callout/__tests__/callout.test.tsx
+++ design-system/components/callout/__tests__/callout.test.tsx
@@ -1,27 +1,19 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { Callout } from '../callout'
 
 describe('Callout', () => {
   it('renders the default variant', () => {
-    const { container, getByTestId } = render(<Callout />)
-
-    expect(container.firstChild).toHaveStyle(
-      'background-color: var(--colors-primary-400)'
-    )
-    expect(getByTestId('information-circle')).toBeInTheDocument()
+    render(<Callout />)
+    expect(screen.getByTestId('information-circle')).toBeInTheDocument()
   })
 
   it('renders the success variant', () => {
-    const { container, getByTestId } = render(<Callout variant="success" />)
-
-    expect(getByTestId('checkmark-circle')).toBeInTheDocument()
-    expect(container.firstChild).toHaveStyle(
-      'background-color: var(--colors-success-400)'
-    )
+    render(<Callout variant="success" />)
+    expect(screen.getByTestId('checkmark-circle')).toBeInTheDocument()
   })
 
   it('renders children', () => {
-    const { getByText } = render(<Callout>Children</Callout>)
-    expect(getByText('Children')).toBeInTheDocument()
+    render(<Callout>Children</Callout>)
+    expect(screen.getByText('Children')).toBeInTheDocument()
   })
 })
