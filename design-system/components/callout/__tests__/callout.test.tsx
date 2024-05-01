25cf9eead8059d7760beea07a0274766c7aa01f1
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2024-05-01 12:51:30 +0200

    test: Rewrite tests to use screen

--- design-system/components/callout/__tests__/callout.test.tsx
+++ design-system/components/callout/__tests__/callout.test.tsx
@@ -4,16 +4,19 @@ import { Callout } from '../callout'
 describe('Callout', () => {
   it('renders the default variant', () => {
     render(<Callout />)
+
     expect(screen.getByTestId('information-circle')).toBeInTheDocument()
   })
 
   it('renders the success variant', () => {
     render(<Callout variant="success" />)
+
     expect(screen.getByTestId('checkmark-circle')).toBeInTheDocument()
   })
 
   it('renders children', () => {
     render(<Callout>Children</Callout>)
+
     expect(screen.getByText('Children')).toBeInTheDocument()
   })
 })
--- design-system/components/code/__tests__/code.test.tsx
+++ design-system/components/code/__tests__/code.test.tsx
@@ -1,55 +1,45 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { Code } from '../code'
 
 describe('Code', () => {
   it('does not render if there is no code string', () => {
-    const { container } = render(<Code codeString="" language="js" />)
+    render(<Code codeString="" language="js" />)
 
-    expect(container.firstChild).not.toBeInTheDocument()
+    expect(screen.queryByRole('code')).not.toBeInTheDocument()
   })
 
   it('renders correctly', () => {
-    const { container } = render(
-      <Code codeString="const foo = 'bar'" language="js" />
-    )
+    render(<Code codeString="const foo = 'bar'" language="js" />)
 
-    expect(container.firstChild).toBeInTheDocument()
+    expect(screen.getByRole('code')).toBeInTheDocument()
   })
 
   it('renders the title if it is passed', () => {
-    const { getByText } = render(
-      <Code codeString="const foo = 'bar'" language="js" title="Title" />
-    )
+    render(<Code codeString="const foo = 'bar'" language="js" title="Title" />)
 
-    expect(getByText('Title')).toBeInTheDocument()
+    expect(screen.getByText('Title')).toBeInTheDocument()
   })
 
   it('renders the correct language', () => {
-    const { getByText } = render(
-      <Code codeString="const foo = 'bar'" language="javascript" />
-    )
+    render(<Code codeString="const foo = 'bar'" language="javascript" />)
 
-    expect(getByText('JAVASCRIPT')).toBeInTheDocument()
+    expect(screen.getByText('JAVASCRIPT')).toBeInTheDocument()
   })
 
   it('does not render line numbers for the terminal variant', () => {
-    const { queryByText } = render(
-      <Code codeString="npm install react" language="bash" />
-    )
+    render(<Code codeString="npm install react" language="bash" />)
 
-    expect(queryByText('1')).not.toBeInTheDocument()
+    expect(screen.queryByText('1')).not.toBeInTheDocument()
   })
 
   it('renders line numbers', () => {
-    const { getByText } = render(
-      <Code codeString="const foo = 'bar'" language="js" />
-    )
+    render(<Code codeString="const foo = 'bar'" language="js" />)
 
-    expect(getByText('1')).toBeInTheDocument()
+    expect(screen.getByText('1')).toBeInTheDocument()
   })
 
   it('highlights the correct line', () => {
-    const { getByText } = render(
+    render(
       <Code
         codeString={`"const foo = 'bar'
         const bar = 'bas'"`}
@@ -58,6 +48,6 @@ describe('Code', () => {
       />
     )
 
-    expect(getByText('2').parentElement).toHaveClass('bg_primary.900')
+    expect(screen.getByText('2').parentElement).toHaveClass('bg_primary.900')
   })
 })
