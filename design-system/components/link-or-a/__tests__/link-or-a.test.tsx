8c1b97a9a64b489d15b14e64025e3d05314d900f
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2024-05-02 09:50:45 +0200

    test: Add tests for the LinkOrA component

---
+++ design-system/components/link-or-a/__tests__/link-or-a.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react'
+import { LinkOrA } from '../link-or-a'
+
+describe('LinkOrA', () => {
+  it('renders the Next link when the href is internal', () => {
+    render(<LinkOrA href="/internal">Internal Link</LinkOrA>)
+
+    const link = screen.getByRole('link')
+
+    expect(link).toHaveAttribute('href', '/internal')
+    expect(link).not.toHaveAttribute('rel', 'nofollow noopener noreferrer')
+    expect(link).not.toHaveAttribute('target', '_blank')
+  })
+
+  it('renders a standard link when the href is external', () => {
+    render(<LinkOrA href="https://external.com">External Link</LinkOrA>)
+
+    const link = screen.getByRole('link')
+
+    expect(link).toHaveAttribute('href', 'https://external.com')
+    expect(link).toHaveAttribute('rel', 'nofollow noopener noreferrer')
+    expect(link).toHaveAttribute('target', '_blank')
+  })
+})
