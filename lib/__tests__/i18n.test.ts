ded1eede1e53c7eb55cce080fb79dc54c564b8fe
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2024-05-08 18:27:42 +0200

    test: Add tests for the i18n functions

---
+++ lib/__tests__/i18n.test.ts
@@ -0,0 +1,70 @@
+// Create test for i18n
+
+import {
+  isDefaultLocale,
+  localizePath,
+  delocalizePath,
+  localizeFileName
+} from '../i18n'
+
+describe('i18n', () => {
+  describe('isDefaultLocale()', () => {
+    it('returns true if locale is default', () => {
+      expect(isDefaultLocale('en')).toBe(true)
+    })
+
+    it('returns false if locale is not default', () => {
+      expect(isDefaultLocale('pl')).toBe(false)
+    })
+
+    it('should return false if locale is not default and default locale is provided', () => {
+      expect(isDefaultLocale('pl', 'en')).toBe(false)
+    })
+  })
+
+  describe('localizePath()', () => {
+    it('returns only path if locale is default', () => {
+      expect(localizePath('en', '/')).toBe('/')
+    })
+
+    it('returns only path if locale is default and default locale is provided', () => {
+      expect(localizePath('pl', '/', 'pl')).toBe('/')
+    })
+
+    it('returns locale + path if locale is not default and is in the path', () => {
+      expect(localizePath('pl', '/pl')).toBe('/pl')
+    })
+
+    it('returns locale + path if locale is not default and is not in the path', () => {
+      expect(localizePath('pl', '/')).toBe('/pl/')
+    })
+
+    it('returns locale + path if locale is not default and default locale is provided', () => {
+      expect(localizePath('pl', '/', 'en')).toBe('/pl/')
+    })
+  })
+
+  describe('delocalizePath()', () => {
+    it('removes locale prefix from path', () => {
+      expect(delocalizePath('pl', '/pl/')).toBe('/')
+    })
+  })
+
+  describe('localizeFileName()', () => {
+    it('returns "index" file name without locale prefix if locale is default', () => {
+      expect(localizeFileName('en')).toBe('index.mdx')
+    })
+
+    it('returns "index" file name with locale prefix if locale is not default', () => {
+      expect(localizeFileName('pl')).toBe('index.pl.mdx')
+    })
+
+    it('returns file name with locale prefix if locale is not default and file name is provided', () => {
+      expect(localizeFileName('pl', 'about')).toBe('about.pl.mdx')
+    })
+
+    it('returns file name with locale prefix if locale is not default and file name and extension are provided', () => {
+      expect(localizeFileName('pl', 'about', 'md')).toBe('about.pl.md')
+    })
+  })
+})
