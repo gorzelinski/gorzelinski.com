bda39f5856bf6aa10c4ca45e7001dd21b08e0986
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2024-05-08 17:46:10 +0200

    test: Add tests for the dictionaries lib

---
+++ lib/__tests__/dictionaries.test.ts
@@ -0,0 +1,27 @@
+import { getDictionary } from '../dictionaries'
+
+describe('getDictionary()', () => {
+  it('returns a dictionary for the English language', async () => {
+    const dictionary = await getDictionary('en')
+
+    expect(dictionary).toMatchObject({
+      component: {
+        logo: {
+          text: 'Matthew Gorzelinski'
+        }
+      }
+    })
+  })
+
+  it('returns a dictionary for the Polish language', async () => {
+    const dictionary = await getDictionary('pl')
+
+    expect(dictionary).toMatchObject({
+      component: {
+        logo: {
+          text: 'Mateusz Gorzeliński'
+        }
+      }
+    })
+  })
+})
