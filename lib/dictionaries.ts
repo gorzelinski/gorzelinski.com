72bfc4c14bc7fc687a0ba1627bdde956fd6d3308
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2024-05-08 17:45:04 +0200

    refactor: Remove special case from the dictionary lib

--- lib/dictionaries.ts
+++ lib/dictionaries.ts
@@ -1,13 +1,12 @@
 import 'server-only'
-import { Locale, i18n } from '@/i18n.config'
+import { Locale } from '@/i18n.config'
 
 const dictionaries = {
   en: () => import('../dictionaries/en.json').then((module) => module.default),
   pl: () => import('../dictionaries/pl.json').then((module) => module.default)
 }
 
-export const getDictionary = async (locale: Locale) =>
-  dictionaries[i18n.locales.includes(locale) ? locale : i18n.defaultLocale]()
+export const getDictionary = async (locale: Locale) => dictionaries[locale]()
 
 export type Dictionary = Awaited<
   ReturnType<(typeof dictionaries)[keyof typeof dictionaries]>
