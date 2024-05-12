570bbefd14b4cb58e3807f9ae0bc0e500c285d5b
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2024-05-12 12:50:04 +0200

    feat: Add a basic Blog Post page layout

--- app/[lang]/blog/[slug]/page.tsx
+++ app/[lang]/blog/[slug]/page.tsx
@@ -1,10 +1,25 @@
-import { MDXRemote } from 'next-mdx-remote/rsc'
 import { Locale } from '@/i18n.config'
-import { localizeFileName } from '@/lib'
-import { getFileByPath } from '@/lib/mdx'
-import { H1 } from '@/design-system'
+import { LINKS } from '@/constants'
+import { formatDate, formatReadingTime } from '@/lib'
+import { getMDX } from '@/lib/mdx'
+import { getDictionary } from '@/lib/dictionaries'
+import { HStack, VStack } from '@/styled-system/jsx'
+import {
+  Article,
+  Figcaption,
+  Figure,
+  H1,
+  Image,
+  Newsletter,
+  P,
+  Pill,
+  Progress,
+  Small,
+  Socials,
+  verticalRhythm
+} from '@/design-system'
 
-export default function Blog({
+export default async function Blog({
   params: { lang, slug }
 }: {
   params: {
@@ -12,17 +27,70 @@ export default function Blog({
     slug: string
   }
 }) {
-  const { content, frontmatter } = getFileByPath(
-    'content',
-    'blog',
-    slug,
-    localizeFileName(lang)
-  )
+  const { component, section, page } = await getDictionary(lang)
+  const { content, frontmatter } = await getMDX<'post'>(LINKS.blog, slug, lang)
 
   return (
     <>
-      <H1>{frontmatter.title}</H1>
-      <MDXRemote source={content}></MDXRemote>
+      <Article>
+        <Progress selector="article" />
+        <header>
+          <Small css={verticalRhythm.marginBottom.s}>
+            {formatDate(frontmatter.date, lang)} •{' '}
+            {formatReadingTime(frontmatter.readingTime.minutes)}{' '}
+            {component.post.min}
+          </Small>
+          <H1 css={verticalRhythm.marginBottom.m}>{frontmatter.title}</H1>
+          <P css={verticalRhythm.marginBottom.m} size="l" color="subtle">
+            {frontmatter.description}
+          </P>
+          <Figure
+            marginX={{
+              base: '-m',
+              sm: '-l',
+              md: '-xl',
+              lg: '-2xl',
+              xl: '-4xl'
+            }}
+            css={verticalRhythm.marginBottom.m}
+            textAlign="center"
+          >
+            <Image
+              aspectRatio="cinema"
+              src={`/images${LINKS.blog}${slug}/${frontmatter.image.src}`}
+              alt={frontmatter.image.alt}
+              width={1200}
+              height={675}
+            />
+            <Figcaption
+              style="italic"
+              textAlign="center"
+              css={verticalRhythm.marginTop.s}
+            >
+              {frontmatter.image.caption}
+            </Figcaption>
+          </Figure>
+        </header>
+        {content}
+        <footer>
+          <VStack
+            alignItems="start"
+            css={{
+              ...verticalRhythm.gap.s,
+              ...verticalRhythm.marginTop['2xmarginBottom']
+            }}
+          >
+            <Pill>
+              {page.blogPost.updated} {formatDate(frontmatter.updated, lang)}
+            </Pill>
+            <HStack css={verticalRhythm.gap.s}>
+              <Small>{page.blogPost.share}</Small>
+              <Socials title={frontmatter.title} />
+            </HStack>
+          </VStack>
+        </footer>
+      </Article>
+      <Newsletter dictionary={section.newsletter} />
     </>
   )
 }
