25ccdcfc32cc9d3de2a4b285070029c151896f03
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2024-05-18 10:26:30 +0200

    test: Add test case for the Socials component

--- design-system/components/socials/__tests__/socials.test.tsx
+++ design-system/components/socials/__tests__/socials.test.tsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react'
+import { LINKS } from '@/constants'
 import { Socials } from '../socials'
 
 describe('Socials', () => {
-  it('renders correctly', () => {
+  it('renders social media links', () => {
     render(
       <Socials
         socials={[
@@ -17,15 +18,29 @@ describe('Socials', () => {
       />
     )
 
-    const socials = screen.getAllByRole('link')
+    const socialLinks = screen.getAllByRole('link')
 
-    expect(socials.length).toEqual(3)
-    socials.forEach((social) => {
+    expect(socialLinks.length).toEqual(3)
+    socialLinks.forEach((social) => {
       expect(social).toHaveAttribute('href')
       expect(social).toHaveAttribute('aria-label')
       expect(social).toHaveAttribute('title')
       expect(social).toHaveAttribute('target', '_blank')
       expect(social).toContainHTML('svg')
+    })
+  })
+
+  it('renders social share links', () => {
+    render(<Socials title="Hello, World!" />)
+
+    const socialLinks = screen.getAllByRole('link')
+
+    expect(socialLinks.length).toEqual(4)
+    socialLinks.forEach((social) => {
+      expect(social).toHaveAttribute(
+        'href',
+        expect.stringContaining(encodeURIComponent(LINKS.siteUrl))
+      )
     })
   })
 })
