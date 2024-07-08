ed60d2ae79f80fe001a655c5707a35f157ae6505
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2024-07-08 08:55:14 +0200

    feat: Add the open graph image to the home page

---
+++ app/[lang]/opengraph-image.tsx
@@ -0,0 +1,36 @@
+import { ImageResponse } from 'next/og'
+import { cookies } from 'next/headers'
+import { CONTENTTYPE, OPENGRAPH } from '@/constants'
+import { PageProps, Theme } from '@/types'
+import { getDictionary, getMetaFont } from '@/scripts'
+import { MetaImage } from '@/design-system'
+
+export const size = OPENGRAPH
+
+export const contentType = CONTENTTYPE
+
+export default async function Image({ params: { lang } }: PageProps) {
+  const { layout, page } = await getDictionary(lang)
+  const theme = cookies().get('theme')?.value || 'light'
+
+  return new ImageResponse(
+    (
+      <MetaImage
+        theme={theme as Theme}
+        title={page.home.metadata.title}
+        subtitle={layout.root.metadata.title}
+      />
+    ),
+    {
+      ...OPENGRAPH,
+      fonts: [
+        await getMetaFont('../../public/fonts/Montserrat-Medium.ttf', {
+          weight: 500
+        }),
+        await getMetaFont('../../public/fonts/Montserrat-SemiBold.ttf', {
+          weight: 600
+        })
+      ]
+    }
+  )
+}
