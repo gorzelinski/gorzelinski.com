1e1c2f3a411be33900557a9c00cf2fe358b79f1a
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2024-07-08 08:58:17 +0200

    feat: Add the open graph image to the blog pages

---
+++ app/[lang]/blog/[slug]/opengraph-image.tsx
@@ -0,0 +1,40 @@
+import { ImageResponse } from 'next/og'
+import { cookies } from 'next/headers'
+import { CONTENTTYPE, LINKS, OPENGRAPH, metadataBase } from '@/constants'
+import { NestedPageProps, Theme } from '@/types'
+import { getDictionary, getMetaFont, getMDX } from '@/scripts'
+import { MetaImage } from '@/design-system'
+
+export const size = OPENGRAPH
+
+export const contentType = CONTENTTYPE
+
+export default async function Image({
+  params: { lang, slug }
+}: NestedPageProps) {
+  const { layout } = await getDictionary(lang)
+  const { frontmatter } = await getMDX<'post'>(LINKS.blog, slug, lang)
+  const theme = cookies().get('theme')?.value || 'light'
+
+  return new ImageResponse(
+    (
+      <MetaImage
+        theme={theme as Theme}
+        title={frontmatter.title}
+        subtitle={layout.root.metadata.title}
+        backgroundURL={`${metadataBase}/images${LINKS.blog}${slug}/${frontmatter.image.src}`}
+      />
+    ),
+    {
+      ...size,
+      fonts: [
+        await getMetaFont('../../public/fonts/Montserrat-Medium.ttf', {
+          weight: 500
+        }),
+        await getMetaFont('../../public/fonts/Montserrat-SemiBold.ttf', {
+          weight: 600
+        })
+      ]
+    }
+  )
+}
