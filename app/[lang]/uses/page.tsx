b1ba8d37685d15ee7eafd611f07bb77eaba0f316
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2024-07-10 13:36:52 +0200

    feat: Add the JSON-LD data to the uses page

--- app/[lang]/uses/page.tsx
+++ app/[lang]/uses/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next'
+import { WebPage, WithContext } from 'schema-dts'
 import { PageProps } from '@/types'
 import { LINKS } from '@/constants'
 import { getMDX, getDictionary } from '@/scripts'
@@ -41,40 +42,90 @@ export async function generateMetadata({
 }
 
 export default async function Uses({ params: { lang } }: PageProps) {
+  const { layout, page } = await getDictionary(lang)
   const { frontmatter, content } = await getMDX(LINKS.uses, '', lang)
+  const jsonLd: WithContext<WebPage> = {
+    '@context': 'https://schema.org',
+    '@type': 'WebPage',
+    inLanguage: lang,
+    name: page.uses.metadata.title,
+    description: page.uses.metadata.description,
+    author: {
+      '@type': 'Person',
+      name: layout.root.metadata.author
+    }
+  }
 
   return (
-    <Article>
-      <H1 css={verticalRhythm.marginBottom.m}>{frontmatter.title}</H1>
-      <P size="l" color="subtle" css={verticalRhythm.marginBottom.m}>
-        {frontmatter.description}
-      </P>
-      <Figure
-        marginX={{
-          base: '-m',
-          sm: '-l',
-          md: '-xl',
-          lg: '-2xl',
-          xl: '-4xl'
-        }}
-        textAlign="center"
-      >
-        <Image
-          aspectRatio="cinema"
-          src={`/images${LINKS.uses}${frontmatter.image.src}`}
-          alt={frontmatter.image.alt}
-          width={1200}
-          height={675}
-        />
-        <Figcaption
-          style="italic"
+    <>
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+      />
+      <Article>
+        <H1 css={verticalRhythm.marginBottom.m}>{frontmatter.title}</H1>
+        <P size="l" color="subtle" css={verticalRhythm.marginBottom.m}>
+          {frontmatter.description}
+        </P>
+        <Figure
+          marginX={{
+            base: '-m',
+            sm: '-l',
+            md: '-xl',
+            lg: '-2xl',
+            xl: '-4xl'
+          }}
           textAlign="center"
-          css={verticalRhythm.marginTop.s}
         >
-          {frontmatter.image.caption}
-        </Figcaption>
-      </Figure>
-      {content}
-    </Article>
+          <Image
+            aspectRatio="cinema"
+            src={`/images${LINKS.uses}${frontmatter.image.src}`}
+            alt={frontmatter.image.alt}
+            width={1200}
+            height={675}
+          />
+          <Figcaption
+            style="italic"
+            textAlign="center"
+            css={verticalRhythm.marginTop.s}
+          >
+            {frontmatter.image.caption}
+          </Figcaption>
+        </Figure>
+        {content}
+      </Article>
+      <Article>
+        <H1 css={verticalRhythm.marginBottom.m}>{frontmatter.title}</H1>
+        <P size="l" color="subtle" css={verticalRhythm.marginBottom.m}>
+          {frontmatter.description}
+        </P>
+        <Figure
+          marginX={{
+            base: '-m',
+            sm: '-l',
+            md: '-xl',
+            lg: '-2xl',
+            xl: '-4xl'
+          }}
+          textAlign="center"
+        >
+          <Image
+            aspectRatio="cinema"
+            src={`/images${LINKS.uses}${frontmatter.image.src}`}
+            alt={frontmatter.image.alt}
+            width={1200}
+            height={675}
+          />
+          <Figcaption
+            style="italic"
+            textAlign="center"
+            css={verticalRhythm.marginTop.s}
+          >
+            {frontmatter.image.caption}
+          </Figcaption>
+        </Figure>
+        {content}
+      </Article>
+    </>
   )
 }
