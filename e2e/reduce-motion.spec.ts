7d563b6ec79b65bab3e863cdf8b9729e01903ffd
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2024-08-07 17:40:11 +0200

    test: Use fixtures in reduce motion e2e tests

--- e2e/reduce-motion.spec.ts
+++ e2e/reduce-motion.spec.ts
@@ -1,31 +1,28 @@
-import { expect, test } from '@playwright/test'
-import { LINKS } from '@/constants'
-import dictionary from '@/dictionaries/en.json'
+import { expect, test } from './fixtures/page'
 
 test.describe('Reduce motion tests', () => {
-  test("checks the 'reduce' motion setting", async ({ page }) => {
-    const {
-      page: { home }
-    } = dictionary
+  test("checks the 'reduce' motion setting", async ({ page, settingsPage }) => {
+    const dictionary = await settingsPage.getDictionary('en')
 
     await page.emulateMedia({ reducedMotion: 'reduce' })
-    await page.goto(LINKS.home)
+    await page.goto(settingsPage.link.home)
 
     await expect(page.getByRole('heading', { level: 1 })).toHaveText(
-      home.landing.typewriter.create
+      dictionary.page.home.landing.typewriter.create
     )
   })
 
-  test("checks the 'no-preference' motion setting", async ({ page }) => {
-    const {
-      page: { home }
-    } = dictionary
+  test("checks the 'no-preference' motion setting", async ({
+    page,
+    settingsPage
+  }) => {
+    const dictionary = await settingsPage.getDictionary('en')
 
     await page.emulateMedia({ reducedMotion: 'no-preference' })
-    await page.goto(LINKS.home)
+    await page.goto(settingsPage.link.home)
 
     await expect(page.getByRole('heading', { level: 1 })).toHaveText(
-      home.landing.typewriter.design
+      dictionary.page.home.landing.typewriter.design
     )
   })
 })
