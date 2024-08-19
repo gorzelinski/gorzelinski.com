d3c0fc1db9a332094e90c79dc7c1b8dfe696db85
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2024-08-19 17:52:29 +0200

    test: Add reading e2e tests

--- e2e/reading.spec.ts
+++ e2e/reading.spec.ts
@@ -1,7 +1,10 @@
 import { test, expect } from './fixtures'
 
-test.describe('Blog tests', () => {
-  test('checks the reading experience', async ({ page, settingsPage }) => {
+test.describe('Reading tests', () => {
+  test('checks the reading experience of a blog post', async ({
+    page,
+    settingsPage
+  }) => {
     const url = `${settingsPage.link.blog}hello-world/`
     const { component, section } = await settingsPage.getDictionary('en')
 
@@ -61,5 +64,53 @@ test.describe('Blog tests', () => {
     await expect(prev).toBeVisible()
     await expect(next).toBeVisible()
     await expect(await related.count()).toBeGreaterThan(0)
+  })
+
+  test('checks the reading experience of a portfolio project', async ({
+    page,
+    settingsPage
+  }) => {
+    const { section } = await settingsPage.getDictionary('en')
+
+    const url = `${settingsPage.link.portfolio}an-lam/`
+
+    await page.goto(url)
+
+    const externalLink = page.getByText('Atomic design')
+    const internalLink = page.getByText('Jamstack')
+
+    const twitter = page.getByRole('link', { name: 'Twitter' })
+    const facebook = page.getByRole('link', { name: 'Facebook' })
+    const linkedin = page.getByRole('link', { name: 'LinkedIn' })
+    const email = page.getByRole('link', { name: settingsPage.link.email })
+
+    const prev = page.locator('a[rel="prev"]')
+    const next = page.locator('a[rel="next"]')
+
+    const newsletter = page.getByText(section.newsletter.heading)
+
+    await expect(externalLink).toHaveAttribute(
+      'rel',
+      'nofollow noopener noreferrer'
+    )
+    await expect(externalLink).toHaveAttribute('target', '_blank')
+
+    await expect(internalLink).not.toHaveAttribute('rel')
+    await expect(internalLink).not.toHaveAttribute('target')
+
+    await expect(twitter).toBeVisible()
+    await expect(twitter).toHaveAttribute('target', '_blank')
+    await expect(facebook).toBeVisible()
+    await expect(facebook).toHaveAttribute('target', '_blank')
+    await expect(linkedin).toBeVisible()
+    await expect(linkedin).toHaveAttribute('target', '_blank')
+    await expect(email).toBeVisible()
+
+    await expect(next).toBeVisible()
+    await expect(newsletter).toBeVisible()
+
+    await next.click()
+
+    await expect(prev).toBeVisible()
   })
 })
