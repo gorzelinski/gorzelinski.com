6a6c42bcaa8e7b46e0c07ad7039918c350bcb3bf
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2024-09-20 14:45:21 +0200

    refactor: Add API to generate meta images

--- lib/index.ts
+++ lib/index.ts
@@ -8,6 +8,7 @@ export {
   localizeFileName
 } from './i18n'
 export { isInternal, getAbsoluteURL, generateAlternateLinks } from './link'
+export { getMetaImage } from './meta-image'
 export { selectActiveClass } from './navigation'
 export { capitalize } from './string'
 export {
