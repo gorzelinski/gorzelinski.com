2f65c6d72430cc7cc3eb66e93bc7503f91bcef18
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2024-09-24 20:39:52 +0200

    fix: Exclude more unnecessary dependencies

--- next.config.js
+++ next.config.js
@@ -5,9 +5,11 @@ const nextConfig = {
     outputFileTracingExcludes: {
       '/*': [
         './public',
-        'node_modules/@swc/core-linux-x64-gnu',
-        'node_modules/@swc/core-linux-x64-musl',
-        'node_modules/@esbuild/linux-x64'
+        './.git',
+        './.next/cache/webpack',
+        './node_modules/@swc/core-linux-x64-gnu',
+        './node_modules/@swc/core-linux-x64-musl',
+        './node_modules/@esbuild/linux-x64'
       ]
     }
   }
