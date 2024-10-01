3fbc14c8d56f104bf9745ff4a29afa9403f052de
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2024-10-01 18:38:55 +0200

    test: Add unit tests to the Confetti component

---
+++ design-system/components/confetti/__tests__/confetti.test.tsx
@@ -0,0 +1,44 @@
+import { act, render, screen } from '@testing-library/react'
+import { Confetti } from '../confetti'
+import 'jest-canvas-mock'
+
+const text = {
+  start: 'More confetti',
+  stop: "That's enough",
+  more: 'MORE!'
+}
+
+describe('Confetti', () => {
+  it('renders correctly', () => {
+    render(<Confetti start={text.start} stop={text.stop} more={text.more} />)
+
+    const button = screen.getByRole('button')
+
+    expect(button).toBeVisible()
+    expect(button).toHaveTextContent(text.start)
+  })
+
+  it('renders stop text after click', () => {
+    render(<Confetti start={text.start} stop={text.stop} more={text.more} />)
+
+    const button = screen.getByRole('button')
+    act(() => {
+      button.click()
+    })
+
+    expect(button).toHaveTextContent(text.stop)
+  })
+
+  it('renders more text after rage-clicking', () => {
+    render(<Confetti start={text.start} stop={text.stop} more={text.more} />)
+
+    const button = screen.getByRole('button')
+    for (let i = 0; i < 12; i++) {
+      act(() => {
+        button.click()
+      })
+    }
+
+    expect(button).toHaveTextContent(text.more)
+  })
+})
