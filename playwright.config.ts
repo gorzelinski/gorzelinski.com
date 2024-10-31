74e68823bd09322ee9497581f944673a40d1b3b4
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2024-10-31 21:42:26 +0100

    style: Format config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -18,12 +18,10 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] }
     },
-
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] }
     },
-
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] }
