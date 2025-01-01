92a2da31f5f822fc100c9b30f0107f182a91448e
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2025-01-01 14:18:44 +0100

    style: Organize link helper functions

--- lib/link.ts
+++ lib/link.ts
@@ -2,20 +2,6 @@ import { Locale, i18n } from '@/i18n.config'
 import { LINKS, metadataBase } from '@/constants'
 import { localizePath } from './i18n'
 
-export function isInternalURL(
-  url: string,
-  siteUrl: string = metadataBase.href
-) {
-  const pageUrl = new URL(siteUrl)
-  const linkUrl = new URL(url, siteUrl)
-
-  return linkUrl.host === pageUrl.host
-}
-
-export function getAbsoluteURL(slug: string = '/', lang: Locale = 'en') {
-  return new URL(localizePath(slug, lang), metadataBase).href
-}
-
 type AlternateLinks = Record<Locale, string>
 
 export function generateAlternateLinks(link: string) {
@@ -26,6 +12,20 @@ export function generateAlternateLinks(link: string) {
     .reduce((acc, curr) => ({ ...acc, ...curr }), {}) as AlternateLinks
 }
 
+export function getAbsoluteURL(slug: string = '/', lang: Locale = 'en') {
+  return new URL(localizePath(slug, lang), metadataBase).href
+}
+
 export function getCoffeeURL(lang: Locale) {
   return lang === 'pl' ? LINKS.buyCoffeeTo : LINKS.buyMeACoffee
+}
+
+export function isInternalURL(
+  url: string,
+  siteUrl: string = metadataBase.href
+) {
+  const pageUrl = new URL(siteUrl)
+  const linkUrl = new URL(url, siteUrl)
+
+  return linkUrl.host === pageUrl.host
 }
