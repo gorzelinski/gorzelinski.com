0e7d812628cb0b9650d61696ea0a02c9642720dc
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2025-02-01 18:12:02 +0100

    test: Configure bigger desktop viewports

--- playwright.config.ts
+++ playwright.config.ts
@@ -16,15 +16,24 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] }
+      use: {
+        ...devices['Desktop Chrome'],
+        viewport: { width: 1440, height: 900 }
+      }
     },
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] }
+      use: {
+        ...devices['Desktop Firefox'],
+        viewport: { width: 1440, height: 900 }
+      }
     },
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'] }
+      use: {
+        ...devices['Desktop Safari'],
+        viewport: { width: 1440, height: 900 }
+      }
     },
     {
       name: 'Mobile Chrome',
