1f72091d9e2882a2341975511794da4b95222a9f
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2025-03-03 19:27:18 +0100

    test: Migrate the Confetti component tests

--- design-system/components/confetti/__tests__/confetti.test.tsx
+++ design-system/components/confetti/__tests__/confetti.test.tsx
@@ -1,12 +1,11 @@
-import { afterEach, describe, expect, it } from 'vitest'
+import { afterEach, describe, expect, it, vi } from 'vitest'
 import { act, cleanup, render, screen } from '@testing-library/react'
 import { Confetti } from '../confetti'
+import dictionary from '@/dictionaries/en.json'
 
-const text = {
-  start: 'More confetti',
-  stop: "That's enough",
-  more: 'MORE!'
-}
+vi.mock('react-confetti', () => ({
+  default: () => null
+}))
 
 describe('Confetti', () => {
   afterEach(() => {
@@ -14,27 +13,45 @@ describe('Confetti', () => {
   })
 
   it('renders correctly', () => {
-    render(<Confetti start={text.start} stop={text.stop} more={text.more} />)
+    render(
+      <Confetti
+        start={dictionary.component.confetti.start}
+        stop={dictionary.component.confetti.stop}
+        more={dictionary.component.confetti.more}
+      />
+    )
 
     const button = screen.getByRole('button')
 
     expect(button).toBeVisible()
-    expect(button).toHaveTextContent(text.start)
+    expect(button).toHaveTextContent(dictionary.component.confetti.start)
   })
 
   it('renders stop text after click', () => {
-    render(<Confetti start={text.start} stop={text.stop} more={text.more} />)
+    render(
+      <Confetti
+        start={dictionary.component.confetti.start}
+        stop={dictionary.component.confetti.stop}
+        more={dictionary.component.confetti.more}
+      />
+    )
 
     const button = screen.getByRole('button')
     act(() => {
       button.click()
     })
 
-    expect(button).toHaveTextContent(text.stop)
+    expect(button).toHaveTextContent(dictionary.component.confetti.stop)
   })
 
   it('renders more text after rage-clicking', () => {
-    render(<Confetti start={text.start} stop={text.stop} more={text.more} />)
+    render(
+      <Confetti
+        start={dictionary.component.confetti.start}
+        stop={dictionary.component.confetti.stop}
+        more={dictionary.component.confetti.more}
+      />
+    )
 
     const button = screen.getByRole('button')
     for (let i = 0; i < 12; i++) {
@@ -43,6 +60,6 @@ describe('Confetti', () => {
       })
     }
 
-    expect(button).toHaveTextContent(text.more)
+    expect(button).toHaveTextContent(dictionary.component.confetti.more)
   })
 })
