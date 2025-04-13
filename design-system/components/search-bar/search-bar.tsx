49b2274494b849c8de591c625851d40846014511
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2025-04-13 10:31:31 +0200

    feat: Add the search bar component

---
+++ design-system/components/search-bar/search-bar.tsx
@@ -0,0 +1,49 @@
+'use client'
+import { useSearchParams, usePathname, useRouter } from 'next/navigation'
+import { useCallback, ChangeEvent } from 'react'
+import { debounce } from '@/lib'
+import { Search, Input, InputWrapper } from '@/design-system'
+import { SearchBarProps } from './search-bar.types'
+
+export const SearchBar = ({ placeholder }: SearchBarProps) => {
+  const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const { replace } = useRouter()
+
+  const handleSearch = useCallback(
+    debounce((term: string) => {
+      const params = new URLSearchParams(searchParams)
+
+      if (term) {
+        params.set('query', term)
+      } else {
+        params.delete('query')
+      }
+
+      replace(`${pathname}?${params.toString()}`)
+    }, 300),
+    [searchParams, pathname, replace]
+  )
+
+  return (
+    <InputWrapper width="stretch">
+      <Input
+        id="search"
+        className="peer"
+        type="search"
+        aria-label={placeholder}
+        placeholder={placeholder}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get('query')?.toString()}
+      />
+      <Search
+        _peerHover={{
+          color: 'gray.500'
+        }}
+        _peerFocus={{
+          color: 'gray.400!'
+        }}
+      />
+    </InputWrapper>
+  )
+} 
