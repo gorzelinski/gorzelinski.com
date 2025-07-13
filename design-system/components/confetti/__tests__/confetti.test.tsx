357672aa3e31f198f37fb0f252c5baa76556af8a
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2025-07-13 11:18:11 +0200

    test: Add missing confetti tests

--- design-system/components/confetti/__tests__/confetti.test.tsx
+++ design-system/components/confetti/__tests__/confetti.test.tsx
@@ -3,9 +3,19 @@ import { act, cleanup, render, screen } from '@testing-library/react'
 import { Confetti } from '../confetti'
 import dictionary from '@/dictionaries/en.json'
 
+let lastConfettiProps: any = null
 vi.mock('react-confetti', () => ({
-  default: () => null
+  default: (props: any) => {
+    lastConfettiProps = props
+    return null
+  }
 }))
+
+const TEST_PROPS = {
+  start: dictionary.component.confetti.start,
+  stop: dictionary.component.confetti.stop,
+  more: dictionary.component.confetti.more
+}
 
 describe('Confetti', () => {
   afterEach(() => {
@@ -14,45 +24,27 @@ describe('Confetti', () => {
   })
 
   it('renders correctly', () => {
-    render(
-      <Confetti
-        start={dictionary.component.confetti.start}
-        stop={dictionary.component.confetti.stop}
-        more={dictionary.component.confetti.more}
-      />
-    )
+    render(<Confetti {...TEST_PROPS} />)
 
     const button = screen.getByRole('button')
 
     expect(button).toBeVisible()
-    expect(button).toHaveTextContent(dictionary.component.confetti.start)
+    expect(button).toHaveTextContent(TEST_PROPS.start)
   })
 
   it('renders stop text after click', () => {
-    render(
-      <Confetti
-        start={dictionary.component.confetti.start}
-        stop={dictionary.component.confetti.stop}
-        more={dictionary.component.confetti.more}
-      />
-    )
+    render(<Confetti {...TEST_PROPS} />)
 
     const button = screen.getByRole('button')
     act(() => {
       button.click()
     })
 
-    expect(button).toHaveTextContent(dictionary.component.confetti.stop)
+    expect(button).toHaveTextContent(TEST_PROPS.stop)
   })
 
   it('renders more text after rage-clicking', () => {
-    render(
-      <Confetti
-        start={dictionary.component.confetti.start}
-        stop={dictionary.component.confetti.stop}
-        more={dictionary.component.confetti.more}
-      />
-    )
+    render(<Confetti {...TEST_PROPS} />)
 
     const button = screen.getByRole('button')
     for (let i = 0; i < 12; i++) {
@@ -61,6 +53,56 @@ describe('Confetti', () => {
       })
     }
 
-    expect(button).toHaveTextContent(dictionary.component.confetti.more)
+    expect(button).toHaveTextContent(TEST_PROPS.more)
+  })
+
+  it('resets state after rage-clicking and shows start text again', () => {
+    render(<Confetti {...TEST_PROPS} />)
+
+    const button = screen.getByRole('button')
+
+    // Click 11 times to reach 'stop' state
+    for (let i = 0; i < 11; i++) {
+      act(() => {
+        button.click()
+      })
+    }
+
+    expect(button).toHaveTextContent(TEST_PROPS.stop) // 'That's enough 🎉'
+
+    // Click 12th time to reach 'more' state
+    act(() => {
+      button.click()
+    })
+
+    expect(button).toHaveTextContent(TEST_PROPS.more) // 'MORE!'
+
+    // Click 13th time, should show 'stop'
+    act(() => {
+      button.click()
+    })
+
+    expect(button).toHaveTextContent(TEST_PROPS.stop) // 'That's enough 🎉'
+
+    // Click 14th time, should reset to 'start'
+    act(() => {
+      button.click()
+    })
+
+    expect(button).toHaveTextContent(TEST_PROPS.start) // 'More confetti 🎉'
+  })
+
+  it('passes correct props to ConfettiComponent', () => {
+    render(<Confetti {...TEST_PROPS} />)
+
+    expect(lastConfettiProps).toEqual(
+      expect.objectContaining({
+        width: expect.any(Number),
+        height: expect.any(Number),
+        recycle: false,
+        numberOfPieces: 200,
+        colors: ['#0466c8', '#47a3ff', '#ffbe0a', '#157f1f', '#c92c4e']
+      })
+    )
   })
 })
