10d15914073b0e08ecb1c2f55c09f0c9e8a3f71b
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2025-12-07 10:40:10 +0100

    test: Refine a11y tests

--- e2e/accessibility.spec.ts
+++ e2e/accessibility.spec.ts
@@ -1,63 +1,57 @@
-import type { Page, ConsoleMessage } from '@playwright/test'
 import AxeBuilder from '@axe-core/playwright'
 import { test, expect, type SettingsPage } from './fixtures'
 
 type PageConfig = {
   name: string
   getUrl: (settingsPage: SettingsPage) => string
-  setup?: (page: Page) => void
 }
 
 test.describe('Accessibility tests', () => {
   const pages: PageConfig[] = [
     {
-      name: 'home page',
+      name: 'home',
       getUrl: (settingsPage) => settingsPage.link.home
     },
     {
-      name: 'portfolio page',
+      name: 'portfolio',
       getUrl: (settingsPage) => settingsPage.link.portfolio
     },
     {
-      name: 'portfolio project page',
+      name: 'portfolio project',
       getUrl: (settingsPage) => `${settingsPage.link.portfolio}an-lam/`
     },
     {
-      name: 'about page',
+      name: 'about',
       getUrl: (settingsPage) => settingsPage.link.about
     },
     {
-      name: 'uses page',
+      name: 'uses',
       getUrl: (settingsPage) => settingsPage.link.uses
     },
     {
-      name: 'blog page',
+      name: 'blog',
       getUrl: (settingsPage) => settingsPage.link.blog
     },
     {
-      name: 'blog post page',
+      name: 'blog post',
       getUrl: (settingsPage) =>
         `${settingsPage.link.blog}object-oriented-programming-in-typescript/`
     },
     {
-      name: 'subscription confirmed page',
+      name: 'subscription confirmed',
       getUrl: (settingsPage) => settingsPage.link.subscriptionConfirmed
     },
     {
-      name: 'not found page',
+      name: 'not found',
       getUrl: () => '/404/'
     }
   ]
 
   pages.forEach((pageConfig) => {
-    test(`${pageConfig.name} should not have accessibility violations`, async ({
+    test(`${pageConfig.name} page should not have accessibility violations`, async ({
       page,
       settingsPage
     }) => {
-      if (pageConfig.setup) {
-        pageConfig.setup(page)
-      }
-
       await page.goto(pageConfig.getUrl(settingsPage))
 
       const accessibilityScanResults = await new AxeBuilder({ page }).analyze()
