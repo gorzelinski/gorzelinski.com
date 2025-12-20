12c08ffdad24bf00156a7976132f6a274d3ab5fa
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2025-12-20 11:16:10 +0100

    feat: Add a Search element

---
+++ design-system/elements/search/search.styles.tsx
@@ -0,0 +1,8 @@
+import { cva } from '@/styled-system/css'
+
+export const search = cva({
+  base: {
+    margin: '0',
+    padding: '0'
+  }
+})
---
+++ design-system/elements/search/search.tsx
@@ -0,0 +1,4 @@
+import { styled } from '@/styled-system/jsx'
+import { search } from './search.styles'
+
+export const Search = styled('search', search)
