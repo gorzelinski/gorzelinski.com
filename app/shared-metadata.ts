022afefa0febdaca1047009bd4f827a0c0371929
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2026-01-29 11:29:50 +0100

    refactor: Use the type keyword in app imports

--- app/shared-metadata.ts
+++ app/shared-metadata.ts
@@ -1,4 +1,4 @@
-import { Metadata } from 'next'
+import type { Metadata } from 'next'
 import type { Locale } from '@/types'
 import { i18n } from '@/i18n.config'
 import { HANDLES } from '@/constants'
