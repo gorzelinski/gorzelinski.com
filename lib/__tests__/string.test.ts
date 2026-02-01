fba8d0f4fce16ce7bf8e084b3975ea58a88f7f74
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2026-02-01 10:16:49 +0100

    style: Organize imports in the string tests

--- lib/__tests__/string.test.ts
+++ lib/__tests__/string.test.ts
@@ -1,5 +1,5 @@
-import { describe, expect, it } from 'vitest'
 import type { ReactNode } from 'react'
+import { describe, expect, it } from 'vitest'
 import { capitalize, getTextFromChildren, slugify } from '../string'
 
 describe('string', () => {
