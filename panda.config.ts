1ff6e61014693cb71cd39a705d03e98d196e1892
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2026-03-28 13:05:03 +0100

    refactor: Move animations to an external file

--- panda.config.ts
+++ panda.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from '@pandacss/dev'
-import { globalCss, semanticTokens, tokens } from './theme'
+import { globalCss, keyframes, semanticTokens, tokens } from './theme'
 
 export default defineConfig({
   preflight: true,
@@ -18,90 +18,7 @@ export default defineConfig({
     tokens,
     semanticTokens,
     extend: {
-      keyframes: {
-        spin: {
-          from: {
-            transform: 'rotate(0deg)'
-          },
-          to: {
-            transform: 'rotate(360deg)'
-          }
-        },
-        wobble: {
-          '16%': {
-            transform: 'translateX(6px)'
-          },
-          '33%': {
-            transform: 'translateX(-5px)'
-          },
-          '49%': {
-            transform: 'translateX(4px)'
-          },
-          '66%': {
-            transform: 'translateX(-2px)'
-          },
-          '83%': {
-            transform: 'translateX(1px)'
-          },
-          '100%': {
-            transform: 'translateX(0)'
-          }
-        },
-        bounceIn: {
-          '20%': {
-            transform: 'translateY(-8px)'
-          },
-          '40%': {
-            transform: 'translateY(1px)'
-          },
-          '60%': {
-            transform: 'translateY(-4px)'
-          },
-          '80%': {
-            transform: 'translateY(0.5px)'
-          },
-          '100%': {
-            transform: 'translateY(0px)'
-          }
-        },
-        buzz: {
-          '10%': {
-            transform: 'translateX(3px) rotate(2deg)'
-          },
-          '20%': {
-            transform: 'translateX(-3px) rotate(-2deg)'
-          },
-          '30%': {
-            transform: 'translateX(3px) rotate(2deg)'
-          },
-          '40%': {
-            transform: 'translateX(-3px) rotate(-2deg)'
-          },
-          '50%': {
-            transform: 'translateX(2px) rotate(1deg)'
-          },
-          '60%': {
-            transform: 'translateX(-2px) rotate(-1deg)'
-          },
-          '70%': {
-            transform: 'translateX(2px) rotate(1deg)'
-          },
-          '80%': {
-            transform: 'translateX(-2px) rotate(-1deg)'
-          },
-          '90%': {
-            transform: 'translateX(1px) rotate(0)'
-          },
-          '100%': {
-            transform: 'translateX(-1px) rotate(0)'
-          }
-        },
-        blink: {
-          '50%': {
-            opacity: 0
-          }
-        }
-      }
+      keyframes
     }
   },
   outdir: 'styled-system'
