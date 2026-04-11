81b3672ea6e17ac918fe78647c10cfc456d940ea
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2026-04-11 09:44:57 +0200

    refactor: Icon creation

--- design-system/icons/cafe.tsx
+++ design-system/icons/cafe.tsx
@@ -1,13 +1,9 @@
-import type { IconProps } from './icon.types'
-import { styled } from '@/styled-system/jsx'
-import { icon } from './icon.styles'
+import { createIcon } from './icon.helpers'
 
-const CafeSVG = (props: IconProps) => (
-  <span {...props}>
-    <svg fill="currentColor" viewBox="0 0 512 512">
-      <path d="M432 64H96a16 16 0 00-16 16v192a96.11 96.11 0 0096 96h112a96.11 96.11 0 0096-96v-80h18a62.07 62.07 0 0062-62V96a32 32 0 00-32-32zm0 66a30 30 0 01-30 30h-18V96h48zM400 400H64a16 16 0 000 32h336a16 16 0 000-32z" />
-    </svg>
-  </span>
+const CafeSvg = () => (
+  <svg fill="currentColor" viewBox="0 0 512 512">
+    <path d="M432 64H96a16 16 0 00-16 16v192a96.11 96.11 0 0096 96h112a96.11 96.11 0 0096-96v-80h18a62.07 62.07 0 0062-62V96a32 32 0 00-32-32zm0 66a30 30 0 01-30 30h-18V96h48zM400 400H64a16 16 0 000 32h336a16 16 0 000-32z" />
+  </svg>
 )
 
-export const Cafe = styled(CafeSVG, icon)
+export const Cafe = createIcon(CafeSvg)
--- design-system/icons/chevron-back.tsx
+++ design-system/icons/chevron-back.tsx
@@ -1,20 +1,16 @@
-import type { IconProps } from './icon.types'
-import { styled } from '@/styled-system/jsx'
-import { icon } from './icon.styles'
+import { createIcon } from './icon.helpers'
 
-const ChevronBackSVG = (props: IconProps) => (
-  <span {...props}>
-    <svg viewBox="0 0 512 512">
-      <path
-        fill="none"
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="48"
-        d="M328 112L184 256l144 144"
-      />
-    </svg>
-  </span>
+const ChevronBackSvg = () => (
+  <svg viewBox="0 0 512 512">
+    <path
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="48"
+      d="M328 112L184 256l144 144"
+    />
+  </svg>
 )
 
-export const ChevronBack = styled(ChevronBackSVG, icon)
+export const ChevronBack = createIcon(ChevronBackSvg)
