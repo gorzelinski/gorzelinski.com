42834163bc839ee64ad40ffa09203ea913e03b8f
Mateusz Gorzeliński <mateusz.gorzelinski@gmail.com>
2026-06-05 17:54:20 +0200

    feat: Add `sizes` constants to the Image component

---
+++ design-system/components/image/image.constants.ts
@@ -0,0 +1,7 @@
+export const IMAGE_SIZES = {
+  avatar: '64px',
+  bleedHero: '(max-width: 783px) 100vw, (max-width: 1279px) 960px, 1104px',
+  columnHalf: '(max-width: 767px) 100vw, (max-width: 1535px) 50vw, 620px',
+  columnThird: '(max-width: 639px) 100vw, (max-width: 1535px) 33vw, 420px',
+  content: '(max-width: 783px) 100vw, 720px'
+} as const
